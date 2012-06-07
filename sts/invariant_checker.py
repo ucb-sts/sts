8f5ec8f05dc87162fb7a0af8146cbeb5ab247e41
Andreas Wundsam <b-github.com@wundsam.net>
2012-06-06 18:55:42 -0700

    invariant_checker: fix whitespace issues

--- sts/invariant_checker.py
+++ sts/invariant_checker.py
@@ -40,7 +40,7 @@ class InvariantChecker(object):
 
   def check_routing_consistency(self):
     pass
-  
+
   def check_correspondence(self, live_switches, live_links, edge_links):
     ''' Return if there were any policy-violations '''
     log.debug("Snapshotting controller...")
@@ -50,7 +50,7 @@ class InvariantChecker(object):
     log.debug("Computing controller omega...")
     controller_omega = self.compute_controller_omega(controller_snapshot, live_switches, live_links, edge_links)
     return self.infer_policy_violations(physical_omega, controller_omega)
-    
+
   # --------------------------------------------------------------#
   #                    HSA utilities                              #
   # --------------------------------------------------------------#
@@ -58,45 +58,45 @@ class InvariantChecker(object):
     (NTF, TTF) = self._get_transfer_functions(live_switches, live_links)
     physical_omega = hsa.compute_omega(NTF, TTF, edge_links)
     return physical_omega
-  
+
   def compute_controller_omega(self, controller_snapshot, live_switches, live_links, edge_links):
     NTF = hsa_topo.NTF_from_snapshot(controller_snapshot, live_switches)
     # Frenetic doesn't store any link or host information.
     # No virtualization though, so we can assume the same TTF. TODO: for now...
     TTF = hsa_topo.generate_TTF(live_links)
     return hsa.compute_omega(NTF, TTF, edge_links)
-  
+
   def compute_single_omega(self, start_link, live_switches, live_links, edge_links):
     (NTF, TTF) = self._get_transfer_functions(live_switches, live_links)
     return hsa.compute_single_omega(NTF, TTF, start_link, edge_links)
-  
+
   def _get_transfer_functions(self, live_switches, live_links):
     NTF = hsa_topo.generate_NTF(live_switches)
     TTF = hsa_topo.generate_TTF(live_links)
     return (NTF, TTF)
-  
+
   def infer_policy_violations(self, physical_omega, controller_omega):
     ''' Return if there were any missing entries '''
     print "# entries in physical omega: %d" % len(physical_omega)
     print "# entries in controller omega: %d" % len(controller_omega)
-    
+
     def get_simple_dict(omega):
       # TODO: ignoring original hs means that we don't account for
       # field modifications, e.g. TTL decreases
       #
       # Omegas are { (original hs, original port) -> [(final hs1, final port1), (final hs2, final port2)...] }
-      # Want to turn them into port -> [(final hs1, final port1), (final hs2, final port2)...] 
-      simple_dict = collections.defaultdict(lambda: set()) 
+      # Want to turn them into port -> [(final hs1, final port1), (final hs2, final port2)...]
+      simple_dict = collections.defaultdict(lambda: set())
       for key, tuples in omega.iteritems():
         (hs, port) = key
         for tup in tuples:
           printable_tup = (str(tup[0]), tup[1])
           simple_dict[port].add(printable_tup)
       return simple_dict
-      
+
     physical_omega = get_simple_dict(physical_omega)
     controller_omega = get_simple_dict(controller_omega)
-    
+
     def print_missing_entries(print_string, omega1, omega2):
       any_missing_entries = False
       for origin_port, final_locations in omega1.iteritems():
@@ -107,10 +107,9 @@ class InvariantChecker(object):
       if not any_missing_entries:
         print "No %s!" % print_string
       return any_missing_entries
-            
+
     # (physical - controller) = missing routing policies
     missing_routing_entries = print_missing_entries("missing routing entries", physical_omega, controller_omega)
     # (controller - physical) = missing ACL policies.
     missing_acl_entries = print_missing_entries("missing acl entries", controller_omega, physical_omega)
     return missing_routing_entries or missing_acl_entries
-    
