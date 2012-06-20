8b04fd64e55551f1b28a6c5dbc357db0fbdc04db
Sam Whitlock <phynominal@gmail.com>
2012-06-20 16:21:22 -0700

    adding functionality checks before netns host is created

--- sts/external_entities.py
+++ sts/external_entities.py
@@ -4,31 +4,50 @@ A factory module to create sockets or other interfaces to edge entities.
 @author samw
 '''
 
-# TODO create a func to serve as a decorator to check for sudo (see mininet)
-
 import itertools
 import socket
 import subprocess
+from os import geteuid
+from exceptions import EnvironmentError
+from platform import system
+from pox.lib.addresses import EthAddr
 
 ETH_P_ALL = 3 # The socket module doesn't have this. From C linux headers
 
 # FIXME does this counter need to be threadsafe? itertools is not...
 _netns_index = itertools.count(0) # for creating unique host device names
 
+def get_eth_address_for_interface(interface):
+  '''Returns an EthAddr object from the interface specified by the argument.
+
+  interface is a string, commonly eth0, wlan0, lo.'''
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
+  return EthAddr(''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1])
+
 def netns(cmd="xterm"):
   '''
   Set up and launch cmd in a new network namespace.
 
-  Returns a tuple of the (socket, Popen object of unshared project in netns)
+  Returns a tuple of the (socket, Popen object of unshared project in netns, EthAddr of guest device).
 
   This method uses functionality that requires CAP_NET_ADMIN capabilites. This
   means that the calling method should check that the python process was
   launched as admin/superuser.
   '''
+
+  if system() != 'Linux':
+    raise EnvironmentError('network namespace functionality requires a Linux environment')
+
+  if (uid = geteuid()) != 0:
+    raise EnvironmentError("superuser privileges required to launch network namespace")
+
   iface_index = _netns_index.next()
 
   host_device = "heth%d" % (iface_index)
   guest_device = "geth%d" % (iface_index)
+
+  guest_eth_addr = get_eth_address_for_interface(guest_device)
 
   try:
     subprocess.check_call(['ip','link','add','name',host_device,'type','veth','peer','name',guest_device])
@@ -45,7 +64,6 @@ def netns(cmd="xterm"):
 
   # all else should have succeeded, so now we fork and unshare for the guest
   guest = subprocess.Popen(["unshare", "-n", cmd])
-  #TODO get this to somehow open up in a screen/tmux session?
 
   # push down the guest device into the netns
   try:
@@ -55,4 +73,4 @@ def netns(cmd="xterm"):
   finally:
     s.close()
 
-  return (s,guest)
+  return (s, guest, guest_eth_addr)
