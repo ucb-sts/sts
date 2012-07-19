27e3fa97bf0fb48081ae4868a27285c631e506ca
Sam Whitlock <phynominal@gmail.com>
2012-07-19 10:42:15 -0700

    adding andi's config file, modified

---
+++ fl-config.py
@@ -0,0 +1,8 @@
+from sts.topology_generator import *
+from sts.experiment_config_lib import *
+controllers = [ Controller(port=6633), Controller(port=6634) ]
+boot_controllers = False
+topology_generator = TopologyGenerator()
+topology_generator.connections_per_switch = 2
+floodlight_port = 8080
+
