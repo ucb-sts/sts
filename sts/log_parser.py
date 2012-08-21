c5f4681d0feb0c691ab717e01c6db46305952bb6
Sam Whitlock <phynominal@gmail.com>
2012-08-21 16:31:57 -0700

    adding basic parsing framework

--- sts/log_parser.py
+++ sts/log_parser.py
@@ -1,1 +1,50 @@
 # Parsing of global log files to create a list of Event objects
+from re import compile, match
+
+def parse_external_event(external_events, id, rest):
+  '''Takes an external event line parsed from the global log file and returns a
+  corresponding ExternalEvent object.'''
+  pass
+
+def parse_internal_event(external_events, dependency_id, rest):
+  '''Takes an internal event line parsed from the global log file and returns
+  a corresponding InternalEvent object.'''
+  pass
+
+event_rgx = compile("(?P<type>e|i) (?P<id>\S+) (?P<rest>.*?)$")
+def parse(logfile_path):
+  '''Input: path to a logfile.
+
+  Output: A list of all the internal and external events in the order in which
+  they exist in the logfile. Each internal event is annotated with the single
+  (for now) external event that it depends on.
+
+  Format for Logfile: Each line is either an internal or external event. Lines
+  that do not match the following specification for either are ignored (with a
+  warning issued).
+
+  * External: ^e ID custom$
+  * Internal: ^i ID_of_external_dependency custom$
+
+  ID can be any unique non-whitespace identifier.'''
+
+  trace = [] # the return value of the parsed log
+  external_events = set() # a set of all external event ids
+
+  parse_function = {
+    'e' : parse_external_event,
+    'i' : parse_internal_event
+    }
+
+  with open(logfile_path, 'r') as log_file:
+    for line in log_file:
+      parsed = match(event_rgx, line)
+      if parsed:
+        parsed = parsed.groupdict()
+        trace.append(parse_function[parsed['type']](
+          external_events,
+          parsed['id'],
+          parsed['rest']
+          )
+
+  return trace
