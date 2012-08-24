be949c37125a93b4150c5406e8ad39b75ddf2556
Sam Whitlock <phynominal@gmail.com>
2012-08-24 10:24:08 -0700

    adding forward id dependency checking in log parsing

--- sts/log_parser.py
+++ sts/log_parser.py
@@ -11,19 +11,31 @@ def check_unique_id(event_id, all_ids):
     raise Exception() # TODO raise a more informative exception
   all_ids.add(event_id)
 
-def parse_external_event(event_ids, event_id, rest):
+def parse_external_event(existing_event_ids, dependent_ids, event_id, rest):
   '''Takes an external event line parsed from the global log file and returns a
   corresponding ExternalEvent object.'''
-  check_unique_id(event_id, event_ids)# TODO pull this out into a decorator
+  check_unique_id(event_id, existing_event_ids)# TODO pull this out into a decorator
+  # TODO parse 'rest' to extract dependent IDs
+  rgx = compile("^\[(?P<dependent_ids>\S+(?: +\S+)*)\] (?P<rest>.*?)$")
+  # ensure that dependent IDs haven't occurred yet
+  parsed = rgx.match(rest)
+
+  if not parsed:
+    raise Exception() # TODO raise a more informative exception
+
+  dependents = set(parsed.group('dependent_ids').split()) # 2 dependent ids could be the same on accident. deal with it!
+  assert(dependents.isdisjoint(existing_event_ids)) # can't have dependencies that have already happened!
+  dependent_ids.update(dependents)
+
   # TODO deal with other external events
 
-def parse_internal_event(events_ids, event_id, rest):
+def parse_internal_event(events_ids, dependent_ids, event_id, rest):
   '''Takes an internal event line parsed from the global log file and returns
   a corresponding InternalEvent object.'''
-  check_unique_id(event_id, event_ids) # TODO pull this out into a decorator
+  check_unique_id(event_id, existing_event_ids) # TODO pull this out into a decorator
+  dependent_ids.discard(event_id)
   # TODO deal with other internal events
 
-event_rgx = compile("(?P<type>e|i) (?P<id>\S+) (?P<rest>.*?)$")
 def parse(logfile_path):
   '''Input: path to a logfile.
 
@@ -35,16 +47,18 @@ def parse(logfile_path):
   that do not match the following specification for either are ignored (with a
   warning issued).
 
-  * External: ^e ID custom$
-  * Internal: ^i ID [list of source IDs] custom$
+  * External: ^e ID [list of dependent IDs] custom$
+  * Internal: ^i ID custom$
   
   ID can be any unique non-whitespace identifier.
 
   The list of source IDs may be empty if the internal event is a source event
   itself. The brackets are part of the literal syntax and not special regex characters.'''
+  event_rgx = compile("(?P<type>[ei]) (?P<id>\S+) (?P<rest>.*?)$")
 
   trace = [] # the return value of the parsed log
   event_ids = set() # a set of all external event ids
+  dependent_ids = set() # dependent ids that have to be satisfied by future internal events
 
   parse_function = {
     'e' : parse_external_event,
@@ -53,14 +67,17 @@ def parse(logfile_path):
 
   with open(logfile_path, 'r') as log_file:
     for line in log_file:
-      parsed = match(event_rgx, line)
+      parsed = event_rgx.match(line)
       if parsed:
         parsed = parsed.groupdict()
         event = parse_function[parsed['type']](
           event_ids,
+          dependent_ids,
           parsed['id'],
           parsed['rest']
           )
         trace.append(event)
+
+  assert(len(dependent_ids) == 0) # all the foward dependencies should be satisfied!
 
   return trace
