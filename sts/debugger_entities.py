3fc8fb98d3b4e527534176fa6040a1d27e62b452
colin-scott <cs@cs.berkeley.edu>
2012-08-27 17:51:09 -0700

    debugger_entities: cleaning up docs

--- sts/debugger_entities.py
+++ sts/debugger_entities.py
@@ -1,20 +1,5 @@
 """
-This module mocks out openflow switches.
-
-This is only for simulation, not emulation. For simluation, we run everything
-in the same process, and mock out switch behavior. This way it's super easy
-to track the state of the system at any point in time (total ordering of events).
-
-Eventually, we want to test the entire stack. We're also going to want to
-allow for the possibility of consistency bugs, which will imply separate
-processes. To emulate, we'll want to :
-    - Boot up MiniNet with some open v switches
-    - inject randomly generated traffic to the switches themselves. This brings up two questions:
-        * Is it possible to package a MiniNet VM as part of POX? That would
-          go against the grain of POX's python-only, pre-packaged everything
-          philosophy.
-        * Can this module successfully interpose on all messages between vswitches
-          and the control application?
+This module mocks out openflow switches, links, and hosts.
 """
 
 from pox.openflow.switch_impl import SwitchImpl, DpPacketOut
@@ -26,11 +11,9 @@ from pox.lib.revent import Event, EventMixin
 import logging
 import pickle
 
-# TODO: model hosts in the network!
-
 class FuzzSwitchImpl (NXSwitchImpl):
   """
-  NOTE: a mock switch implementation for testing purposes. Can simulate dropping dead.
+  A mock switch implementation for testing purposes. Can simulate dropping dead.
   """
   def __init__ (self, create_io_worker, dpid, name=None, ports=4, miss_send_len=128,
                 n_buffers=100, n_tables=1, capabilities=None):
@@ -56,7 +39,7 @@ class FuzzSwitchImpl (NXSwitchImpl):
 
   def connect(self):
     # NOTE: create_io_worker is /not/ an instancemethod but just a function
-    # so we have to pass in the self parameter explicitely
+    # so we have to pass in the self parameter explicitly
     for info in self.controller_info:
       io_worker = self.create_io_worker(self, info)
       conn = self.set_io_worker(io_worker)
@@ -64,7 +47,7 @@ class FuzzSwitchImpl (NXSwitchImpl):
       conn.error_handler = self.error_handler
 
   def fail(self):
-    # TODO: depending on the type of failure, a real switch failure
+    # TODO(cs): depending on the type of failure, a real switch failure
     # might not lead to an immediate disconnect
     if self.failed:
       self.log.warn("Switch already failed")
@@ -84,11 +67,11 @@ class FuzzSwitchImpl (NXSwitchImpl):
 
   def serialize(self):
     # Skip over non-serializable data, e.g. sockets
-    # TODO: is self.log going to be a problem?
+    # TODO(cs): is self.log going to be a problem?
     serializable = MockOpenFlowSwitch(self.dpid, self.parent_controller_name)
     # Can't serialize files
     serializable.log = None
-    # TODO: need a cleaner way to add in the NOM port representation
+    # TODO(cs): need a cleaner way to add in the NOM port representation
     if self.switch_impl:
       serializable.ofp_phy_ports = self.switch_impl.ports.values()
     return pickle.dumps(serializable, protocol=0)
