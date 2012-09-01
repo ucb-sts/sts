4d0966f46570c81ad32fd8e4742fff91f4e9530a
colin-scott <cs@cs.berkeley.edu>
2012-09-01 10:36:28 -0700

    sts.topology: line width fixes

--- sts/topology.py
+++ sts/topology.py
@@ -36,8 +36,8 @@ logger = logging.getLogger("sts.topology")
 def create_switch(switch_id, num_ports):
   ports = []
   for port_no in range(1, num_ports+1):
-    port = ofp_phy_port( port_no=port_no,
-                          hw_addr=EthAddr("00:00:00:00:%02x:%02x" % (switch_id, port_no)) )
+    eth_addr = EthAddr("00:00:00:00:%02x:%02x" % (switch_id, port_no))
+    port = ofp_phy_port( port_no=port_no, hw_addr=eth_addr )
     # monkey patch an IP address onto the port for anteater purposes
     port.ip_addr = "1.1.%d.%d" % (switch_id, port_no)
     ports.append(port)
@@ -45,14 +45,17 @@ def create_switch(switch_id, num_ports):
   def unitialized_io_worker(switch):
     raise SystemError("Not initialialized")
 
-  return FuzzSwitchImpl(create_io_worker=unitialized_io_worker, dpid=switch_id, name="SoftSwitch(%d)" % switch_id, ports=ports)
+  return FuzzSwitchImpl(create_io_worker=unitialized_io_worker,
+                        dpid=switch_id, name="SoftSwitch(%d)" % switch_id,
+                        ports=ports)
 
 def get_switchs_host_port(switch):
   ''' return the switch's ofp_phy_port connected to the host '''
   # We wire up the last port of the switch to the host
   return switch.ports[sorted(switch.ports.keys())[-1]]
 
-def create_host(ingress_switch_or_switches, mac_or_macs=None, ip_or_ips=None, get_switch_port=get_switchs_host_port):
+def create_host(ingress_switch_or_switches, mac_or_macs=None, ip_or_ips=None,
+                get_switch_port=get_switchs_host_port):
   ''' Create a Host, wired up to the given ingress switches '''
   switches = ingress_switch_or_switches
   if type(switches) != list:
@@ -86,7 +89,8 @@ def create_host(ingress_switch_or_switches, mac_or_macs=None, ip_or_ips=None, ge
 
   name = "host:" + ",".join(map(lambda interface: "%s" % str(interface.ips), interfaces))
   host = Host(interfaces, name)
-  access_links = [ AccessLink(host, interface, switch, get_switch_port(switch)) for interface, switch in interface_switch_pairs ]
+  access_links = [ AccessLink(host, interface, switch, get_switch_port(switch))
+                   for interface, switch in interface_switch_pairs ]
   return (host, access_links)
 
 class PatchPanel(object):
@@ -215,7 +219,7 @@ class Topology(object):
   def ok_to_send(self, dp_event):
     """Returns True if it is ok to send the dp_event arg."""
     (next_hop, next_port) = self.get_connected_port(dp_event.switch,
-                                                               dp_event.port)
+                                                    dp_event.port)
     if type(dp_event.node) == Host or type(next_hop) == Host:
       # TODO(cs): model access link failures:
       return True
@@ -283,25 +287,28 @@ class Topology(object):
 
   def connect_to_controllers(self, controller_info_list, io_worker_generator):
     '''
-    Bind sockets from the switch_impls to the controllers. For now, assign each switch to the next
-    controller in the list in a round robin fashion.
+    Bind sockets from the switch_impls to the controllers. For now, assign each
+    switch to the next controller in the list in a round robin fashion.
 
-    Controller info list is a list of (controller ip address, controller port number) tuples
+    Controller info list is a list of
+    (controller ip address, controller port number) tuples
 
     Return a list of socket objects
     '''
     controller_info_cycler = itertools.cycle(controller_info_list)
     connections_per_switch = len(controller_info_list)
 
-    logger.debug("Connecting %d switches to %d controllers (setting up %d conns per switch)..." %
-            (len(self.switches), len(controller_info_list), connections_per_switch))
+    logger.debug('''Connecting %d switches to %d controllers (setting up %d'''
+                 ''' conns per switch)...''' %
+                (len(self.switches), len(controller_info_list), connections_per_switch))
 
     for (idx, switch_impl) in enumerate(self.switches):
       if len(self.switches) < 20 or not idx % 250:
         logger.debug("Connecting switch %d / %d" % (idx, len(self.switches)))
 
       def create_io_worker(switch, controller_info):
-        controller_socket = connect_socket_with_backoff(controller_info.address, controller_info.port)
+        controller_socket = connect_socket_with_backoff(controller_info.address,
+                                                        controller_info.port)
         # Set non-blocking
         controller_socket.setblocking(0)
         return io_worker_generator(controller_socket)
@@ -320,15 +327,17 @@ class Topology(object):
   @staticmethod
   def populate_from_topology(graph):
      """
-     Take a pox.lib.graph.graph.Graph and generate arguments needed for the simulator
+     Take a pox.lib.graph.graph.Graph and generate arguments needed for the
+     simulator
      """
      topology = Topology()
      topology.hosts = graph.find(is_a=Host)
      topology.switches = graph.find(is_a=SwitchImpl)
      topology.access_links = [AccessLink(host, switch[0], switch[1][0], switch[1][1])
                               for host in hosts
-                              for switch in filter(lambda n: isinstance(n[1][0], SwitchImpl),
-                              graph.ports_for_node(host).iteritems())]
+                              for switch in filter(
+                                  lambda n: isinstance(n[1][0], SwitchImpl),
+                                  graph.ports_for_node(host).iteritems())]
      return topology
 
 class MeshTopology(Topology):
@@ -339,11 +348,13 @@ class MeshTopology(Topology):
     '''
     Topology.__init__(self)
 
-    # Every switch has a link to every other switch + 1 host, for N*(N-1)+N = N^2 total ports
+    # Every switch has a link to every other switch + 1 host,
+    # for N*(N-1)+N = N^2 total ports
     ports_per_switch = (num_switches - 1) + 1
 
     # Initialize switches
-    self.switches = [ create_switch(switch_id, ports_per_switch) for switch_id in range(1, num_switches+1) ]
+    self.switches = [ create_switch(switch_id, ports_per_switch)
+                      for switch_id in range(1, num_switches+1) ]
     host_access_link_pairs = [ create_host(switch) for switch in self.switches ]
     self.hosts = map(lambda pair: pair[0], host_access_link_pairs)
     access_link_list_list = map(lambda pair: pair[1], host_access_link_pairs)
@@ -356,9 +367,11 @@ class MeshTopology(Topology):
     self.network_links = link_topology.get_network_links()
 
   class FullyMeshedLinks(object):
-    """ A factory method (inner class) for creating a fully meshed network. Connects every pair
-        of switches. Ports are in ascending order of the dpid of connected switch, while skipping
-        the self-connections. I.e., for (dpid, portno):
+    """
+    A factory method (inner class) for creating a fully meshed network.
+    Connects every pair of switches. Ports are in ascending order of
+    the dpid of connected switch, while skipping the self-connections.
+    I.e., for (dpid, portno):
         (0, 0) <-> (1,0)
         (2, 1) <-> (1,1)
     """
@@ -374,7 +387,8 @@ class MeshTopology(Topology):
         self.interface2access_link[access_link.interface] = access_link
 
     def get_connected_port(self, node, port):
-      ''' Given a node and a port, return a tuple (node, port) that is directly connected to the port '''
+      ''' Given a node and a port, return a tuple (node, port) that is directly
+      connected to the port '''
       if port in self.port2access_link:
         access_link = self.port2access_link[port]
         return (access_link.host, access_link.interface)
@@ -458,7 +472,8 @@ class FatTree (Topology):
         self.edges.append(edge_switch)
 
       edge = self.edges[-1]
-      # edge ports 1 through (k/2) are dedicated to hosts, and (k/2)+1 through k are for agg
+      # edge ports 1 through (k/2) are dedicated to hosts, and (k/2)+1 through
+      # k are for agg
       edge_port_no = (i % self.hosts_per_edge) + 1
       # We give it a portland pseudo mac, just for giggles
       # Slightly modified from portland (no vmid, assume 8 bit pod id):
@@ -466,12 +481,15 @@ class FatTree (Topology):
       # position and pod are 0-indexed, port is 1-indexed
       position = (len(self.edges)-1) % self.edge_per_pod
       edge.position = position
-      portland_mac = EthAddr("00:00:00:%02x:%02x:%02x" % (current_pod_id, position, edge_port_no))
-      # Uhh, unfortunately, OpenFlow 1.0 doesn't support prefix matching on MAC addresses.
-      # So we do prefix matching on IP addresses, which yields exactly the same # of flow
-      # entries for HSA, right?
-      portland_ip_addr = IPAddr("123.%d.%d.%d" % (current_pod_id, position, edge_port_no))
-      (host, host_access_links) = create_host(edge, portland_mac, portland_ip_addr, lambda switch: switch.ports[edge_port_no])
+      portland_mac = EthAddr("00:00:00:%02x:%02x:%02x" %
+                             (current_pod_id, position, edge_port_no))
+      # Uhh, unfortunately, OpenFlow 1.0 doesn't support prefix matching on MAC
+      # addresses. So we do prefix matching on IP addresses, which yields
+      # exactly the same # of flow entries for HSA, right?
+      portland_ip_addr = IPAddr("123.%d.%d.%d" %
+                                (current_pod_id, position, edge_port_no))
+      (host, host_access_links) = create_host(edge, portland_mac, portland_ip_addr,
+                                              lambda switch: switch.ports[edge_port_no])
       host.pod_id = current_pod_id
       self.hosts.append(host)
       self.access_links = self.access_links.union(set(host_access_links))
@@ -487,15 +505,18 @@ class FatTree (Topology):
         agg.pod_id = pod_id
         current_aggs.append(agg)
 
-      current_edges = self.edges[pod_id * self.edge_per_pod : (pod_id * self.edge_per_pod) + self.edge_per_pod]
+      current_edges = self.edges[pod_id * self.edge_per_pod : \
+                                (pod_id * self.edge_per_pod) + self.edge_per_pod]
       # edge ports (k/2)+1 through k connect to aggs
       # agg port k+1 connects to edge switch k in the pod
       current_agg_port_no = 1
       for edge in current_edges:
         current_edge_port_no = (k/2)+1
         for agg in current_aggs:
-          self.network_links.add(Link(edge, edge.ports[current_edge_port_no], agg, agg.ports[current_agg_port_no]))
-          self.network_links.add(Link(agg, agg.ports[current_agg_port_no], edge, edge.ports[current_edge_port_no]))
+          self.network_links.add(Link(edge, edge.ports[current_edge_port_no],
+                                      agg, agg.ports[current_agg_port_no]))
+          self.network_links.add(Link(agg, agg.ports[current_agg_port_no],
+                                      edge, edge.ports[current_edge_port_no]))
           current_edge_port_no += 1
         current_agg_port_no += 1
 
@@ -515,21 +536,25 @@ class FatTree (Topology):
       for agg_port_no in range(k/2+1, k+1):
         core = core_cycler.next()
         core_port_no = agg.pod_id + 1
-        self.network_links.add(Link(agg, agg.ports[agg_port_no], core, core.ports[core_port_no]))
-        self.network_links.add(Link(core, core.ports[core_port_no], agg, agg.ports[agg_port_no]))
+        self.network_links.add(Link(agg, agg.ports[agg_port_no], core,
+                                    core.ports[core_port_no]))
+        self.network_links.add(Link(core, core.ports[core_port_no], agg,
+                                    agg.ports[agg_port_no]))
 
     self.switches = self.cores + self.aggs + self.edges
     self.wire_tree()
     self._sanity_check_tree()
-    logger.debug("Fat tree construction: done (%d cores, %d aggs, %d edges, %d hosts)" %
-        (len(self.cores), len(self.aggs), len(self.edges), len(self.hosts)))
+    logger.debug('''Fat tree construction: done (%d cores, %d aggs,'''
+                 '''%d edges, %d hosts)''' %
+                 (len(self.cores), len(self.aggs), len(self.edges), len(self.hosts)))
 
   def wire_tree(self):
     # Auxiliary data to make get_connected_port efficient
     self.port2internal_link = {}
     for link in self.network_links:
       #if link.start_port in self.port2internal_link:
-        #raise RuntimeError("%s Already there %s" % (str(link), str(self.port2internal_link[link.start_port])))
+        #raise RuntimeError("%s Already there %s" % (str(link),
+        #                   str(self.port2internal_link[link.start_port])))
       self.port2internal_link[link.start_port] = link
 
     # TODO(cs): this should be in a unit test, not here
@@ -573,23 +598,26 @@ class FatTree (Topology):
 
   def install_default_routes(self):
     '''
-    Install static routes as proposed in Section 3 of "A Scalable, Commodity Data Center Network Architecture".
+    Install static routes as proposed in Section 3 of
+    "A Scalable, Commodity Data Center Network Architecture".
     This is really a strawman routing scheme. PORTLAND, et. al. are much better
     '''
     pass
 
   def install_portland_routes(self):
     '''
-    We use a modified version of PORTLAND. We make two changes: OpenFlow 1.0 doesn't support prefix matching
-    on MAC addresses, so we use IP addresses instead. (same # of flow entries, and we don't model flooding anyway)
+    We use a modified version of PORTLAND. We make two changes: OpenFlow 1.0
+    doesn't support prefix matching on MAC addresses, so we use IP addresses
+    instead. (same # of flow entries, and we don't model flooding anyway)
     Second, we ignore vmid and assume 8 bit pod ids. So, IPs are of the form:
     123.pod.position.port
     '''
     k = self.k
 
     for core in self.cores:
-      # When forwarding a packet, the core switch simply inspects the bits corresponding to the pod
-      # number in the PMAC destination address to determine the appropriate output port.
+      # When forwarding a packet, the core switch simply inspects the bits
+      # corresponding to the pod number in the PMAC destination address to
+      # determine the appropriate output port.
       for port_no in core.ports.keys():
         # port_no i+1 corresponds to pod i
         match = ofp_match(nw_dst="123.%d.0.0/16" % (port_no-1))
@@ -610,8 +638,8 @@ class FatTree (Topology):
         flow_mod = ofp_flow_mod(match=match, actions=[ofp_action_output(port=port_no)])
         agg._receive_flow_mod(flow_mod)
 
-      # We model load balancing to uplinks as a single flow entry -- h/w supports ecmp efficiently
-      # while only requiring a single TCAM entry
+      # We model load balancing to uplinks as a single flow entry -- h/w supports
+      # ecmp efficiently while only requiring a single TCAM entry
       match = ofp_match(nw_dst="123.0.0.0/8")
       # Forward to the right-most uplink
       flow_mod = ofp_flow_mod(match=match, actions=[ofp_action_output(port=k)])
@@ -627,15 +655,16 @@ class FatTree (Topology):
         flow_mod = ofp_flow_mod(match=match, actions=[ofp_action_output(port=port_no)])
         edge._receive_flow_mod(flow_mod)
 
-      # We model load balancing to uplinks as a single flow entry -- h/w supports ecmp efficiently
-      # while only requiring a single TCAM entry
+      # We model load balancing to uplinks as a single flow entry --
+      # h/w supports ecmp efficiently while only requiring a single TCAM entry
       match = ofp_match(nw_dst="123.0.0.0/8")
       # Forward to the right-most uplink
       flow_mod = ofp_flow_mod(match=match, actions=[ofp_action_output(port=k)])
       edge._receive_flow_mod(flow_mod)
 
   def get_connected_port(self, node, port):
-    ''' Given a node and a port, return a tuple (node, port) that is directly connected to the port '''
+    ''' Given a node and a port, return a tuple (node, port) that is directly
+    connected to the port '''
     # TODO(cs): use a $@#*$! graph library
     if port in self.port2access_link:
       access_link = self.port2access_link[port]
