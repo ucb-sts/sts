41471c0399e50fcafbb3af4d4418df010e1e5f10
Andreas Wundsam <b-github.com@wundsam.net>
2012-09-11 20:49:23 -0700

    add the troubleshoot_pox_l2_learning trace to debug POX l2 learning

---
+++ config/troubleshoot_pox_l2_learning.py
@@ -0,0 +1,11 @@
+
+from experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow import Replayer
+
+controllers = [ControllerConfig(cmdline="./pox.py --no-cli openflow.of_01 --address=__address__ --port=__port__ topology forwarding.l2_learning", cwd="pox", address="127.0.0.1",port=8888)]
+topology_class = MeshTopology
+topology_params = "num_switches=2"
+patch_panel_class = BufferedPatchPanel
+control_flow = Replayer("input_traces/troubleshoot_pox_l2_learning.trace")
+dataplane_trace = "dataplane_traces/ping_pong_same_subnet.trace"
