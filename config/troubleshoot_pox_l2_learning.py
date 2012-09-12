f6caef6ffa3b377cf92ce70a6b0afb2fccbcd2e9
Andreas Wundsam <b-github.com@wundsam.net>
2012-09-12 02:30:37 -0700

    updated troubleshoot l2_learning trace

--- config/troubleshoot_pox_l2_learning.py
+++ config/troubleshoot_pox_l2_learning.py
@@ -3,7 +3,7 @@ from experiment_config_lib import ControllerConfig
 from sts.topology import *
 from sts.control_flow import Replayer
 
-controllers = [ControllerConfig(cmdline="./pox.py --no-cli openflow.of_01 --address=__address__ --port=__port__ samples.topo forwarding.l2_learning", cwd="pox", address="127.0.0.1",port=8888)]
+controllers = [ControllerConfig(cmdline="./pox.py --no-cli openflow.of_01 --address=__address__ --port=__port__ samples.topo forwarding.l2_learning",address="127.0.0.1",port=8888, cwd="pox")]
 topology_class = MeshTopology
 topology_params = "num_switches=2"
 patch_panel_class = BufferedPatchPanel
