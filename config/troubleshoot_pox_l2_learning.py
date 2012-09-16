6b97a886a09b65e0b084a448e19b2fd43b688693
colin-scott <cs@cs.berkeley.edu>
2012-09-15 22:37:10 -0700

    Updating troubleshoot_pox_l2 config

--- config/troubleshoot_pox_l2_learning.py
+++ config/troubleshoot_pox_l2_learning.py
@@ -3,9 +3,9 @@ from experiment_config_lib import ControllerConfig
 from sts.topology import *
 from sts.control_flow import Replayer
 
-controllers = [ControllerConfig(cmdline="./pox.py --no-cli openflow.of_01 --address=__address__ --port=__port__ samples.topo forwarding.l2_learning",address="127.0.0.1",port=8888, cwd="pox")]
+controllers = [ControllerConfig(cmdline='./pox.py --no-cli openflow.of_01 --address=__address__ --port=__port__ samples.topo forwarding.l2_learning', address='127.0.0.1', port=8888, cwd='pox')]
 topology_class = MeshTopology
 topology_params = "num_switches=2"
 patch_panel_class = BufferedPatchPanel
-control_flow = Replayer("input_traces/troubleshoot_pox_l2_learning.trace")
+control_flow = Replayer("input_traces/2012_09_15_22_33_10.trace")
 dataplane_trace = "dataplane_traces/ping_pong_same_subnet.trace"
