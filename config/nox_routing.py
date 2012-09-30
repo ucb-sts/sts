43f37fb0b66c483c3462cd132da3954e09102768
colin-scott <cs@cs.berkeley.edu>
2012-09-29 22:28:25 -0700

    Lowering fuzzer params

--- config/nox_routing.py
+++ config/nox_routing.py
@@ -1,5 +1,5 @@
 from experiment_config_lib import ControllerConfig
-from sts.topology import MeshTopology, BufferedPatchPanel
+from sts.topology import FatTree, BufferedPatchPanel
 from sts.control_flow import Fuzzer
 from sts.input_traces.input_logger import InputLogger
 from sts.invariant_checker import InvariantChecker
@@ -10,10 +10,7 @@ controllers = [ControllerConfig(command_line, cwd="nox_classic/build/src", addre
 
 # Use a FatTree with 4 pods (already the default)
 # (specify the class, but don't instantiate the object)
-topology_class = MeshTopology
-# Comma-delimited list of arguments to pass into the FatTree constructor,
-# specified just as you would type them within the parens.
-topology_params = "num_switches=2"
+topology_class = FatTree
 
 # Use a BufferedPatchPanel (already the default)
 # (specify the class, but don't instantiate the object)
@@ -21,9 +18,10 @@ patch_panel_class = BufferedPatchPanel
 
 # Use a Fuzzer (already the default)
 control_flow = Fuzzer(input_logger=InputLogger(),
+                      check_interval=80,
                       invariant_check=InvariantChecker.check_connectivity)
 
 # Specify None as the dataplane trace (already the default)
 # Otherwise, specify the path to the trace file
 # (e.g. "dataplane_traces/ping_pong_same_subnet.trace")
-dataplane_trace = "dataplane_traces/ping_pong_same_subnet.trace"
+dataplane_trace = "dataplane_traces/ping_pong_fat_tree.trace"
