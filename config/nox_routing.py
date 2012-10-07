42eed887573e8b79e2ad8729db84629fd65bcd5f
colin-scott <cs@cs.berkeley.edu>
2012-10-07 12:34:11 -0700

    Tweaking fuzzer params and nox routing

--- config/nox_routing.py
+++ config/nox_routing.py
@@ -1,11 +1,11 @@
 from experiment_config_lib import ControllerConfig
-from sts.topology import FatTree, BufferedPatchPanel
-from sts.control_flow import Fuzzer
+from sts.topology import FatTree, MeshTopology, BufferedPatchPanel
+from sts.control_flow import Fuzzer,Interactive
 from sts.input_traces.input_logger import InputLogger
 from sts.invariant_checker import InvariantChecker
 
 # Use POX as our controller
-command_line = "./nox_core -i ptcp:6633 simplerouting"
+command_line = "./nox_core -i ptcp:6633 routing"
 controllers = [ControllerConfig(command_line, cwd="nox_classic/build/src", address="127.0.0.1", port=6633)]
 
 # Use a FatTree with 4 pods (already the default)
@@ -18,8 +18,8 @@ patch_panel_class = BufferedPatchPanel
 
 # Use a Fuzzer (already the default)
 control_flow = Fuzzer(input_logger=InputLogger(),
-                      check_interval=80,
-                      invariant_check=InvariantChecker.check_connectivity)
+                           check_interval=80,
+                           invariant_check=InvariantChecker.check_connectivity)
 
 # Specify None as the dataplane trace (already the default)
 # Otherwise, specify the path to the trace file
