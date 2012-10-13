e45c54d4c462219a71befacc275d239fb27122b9
Andreas Wundsam <b-github.com@wundsam.net>
2012-10-12 18:53:16 -0700

    add a config that creates a violation

---
+++ config/create_violation.py
@@ -0,0 +1,11 @@
+
+from experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow import Replayer
+
+controllers = [ControllerConfig(cmdline='./pox.py --no-cli --verbose openflow.of_01 --address=__address__ --port=__port__ sts.syncproto.pox_syncer samples.topo forwarding.l2_learning', address='127.0.0.1', port=8888, cwd='pox', sync='tcp:localhost:18888')]
+topology_class = MeshTopology
+topology_params = "num_switches=2"
+patch_panel_class = BufferedPatchPanel
+control_flow = Replayer("input_traces/create_violation.trace")
+dataplane_trace = "dataplane_traces/ping_pong_same_subnet.trace"
