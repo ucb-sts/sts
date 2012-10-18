0c19c7eb290a81771fbd07332cfe6fb1f365a14a
colin-scott <cs@cs.berkeley.edu>
2012-10-18 14:57:51 -0700

    Cleaning up fat_tree config file

--- config/fuzz_pox_fattree.py
+++ config/fuzz_pox_fattree.py
@@ -4,16 +4,15 @@ from sts.control_flow import Interactive, Fuzzer
 from sts.input_traces.input_logger import InputLogger
 
 # Use POX as our controller
-#command_line = "./pox.py --verbose --no-cli openflow.of_01 --address=__address__ --port=__port__ sts.syncproto.pox_syncer samples.topo forwarding.l2_learning messenger.messenger samples.nommessenger"
 command_line = "./pox.py --verbose --no-cli openflow.of_01 --address=__address__ --port=__port__ sts.syncproto.pox_syncer samples.topo forwarding.l2_multi messenger.messenger samples.nommessenger"
 controllers = [ControllerConfig(command_line, cwd="pox", sync="tcp:localhost:18899")]
 
 # Use a FatTree with 4 pods (already the default)
 # (specify the class, but don't instantiate the object)
-topology_class = FatTree #, MeshTopology
+topology_class = FatTree
 # Comma-delimited list of arguments to pass into the FatTree constructor,
 # specified just as you would type them within the parens.
-#topology_params = "num_switches=2"
+topology_params = ""
 
 # Use a BufferedPatchPanel (already the default)
 # (specify the class, but don't instantiate the object)
