d3953b703fc750000e43d7d07ff46c4af858c110
colin-scott <cs@cs.berkeley.edu>
2012-10-18 15:08:53 -0700

    Making pox_list_violation.py a smaller run on mesh, not FatTree

--- config/pox_list_violation.py
+++ config/pox_list_violation.py
@@ -4,8 +4,8 @@ from sts.topology import *
 from sts.control_flow import Replayer
 
 controllers = [ControllerConfig(cmdline='./pox.py --verbose --no-cli openflow.of_01 --address=__address__ --port=__port__ sts.syncproto.pox_syncer samples.topo forwarding.l2_multi messenger.messenger samples.nommessenger', address='127.0.0.1', port=8888, cwd='pox', sync='tcp:localhost:18899')]
-topology_class = FatTree
-topology_params = ""
+topology_class = MeshTopology
+topology_params = "num_switches=2"
 patch_panel_class = BufferedPatchPanel
-control_flow = Replayer("input_traces/pox_list_violation.trace")
-dataplane_trace = "dataplane_traces/ping_pong_fat_tree.trace"
+control_flow = Replayer("input_traces/2012_10_18_14_59_36.trace")
+dataplane_trace = "dataplane_traces/ping_pong_same_subnet.trace"
