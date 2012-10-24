d9f020def60db065813543a7b19e71eb5dfb10ac
colin-scott <cs@cs.berkeley.edu>
2012-10-24 12:50:37 -0700

    Add unit test for match_fingerprints

--- tests/unit/sts/event_dag_test.py
+++ tests/unit/sts/event_dag_test.py
@@ -16,6 +16,14 @@ class MockEvent(Event):
   def proceed(self, simulation):
     pass
 
+class MockInternalEvent(InternalEvent):
+  def __init__(self, fingerprint, label="a"):
+    self.fingerprint = fingerprint
+    self.label = label
+
+  def proceed(self, simulation):
+    pass
+
 class event_dag_test(unittest.TestCase):
   def test_split_basic(self):
     events = [MockEvent(), MockEvent(), MockEvent(), MockEvent()]
@@ -57,6 +65,50 @@ class event_dag_test(unittest.TestCase):
     splits = dag.split_inputs(3)
     self.assertEqual(3, len(splits))
     self.assertEqual(3, len(splits[0]) + len(splits[1]) + len(splits[2]))
+
+  def test_match_fingerprints_simple(self):
+    expected = [ MockInternalEvent(fingerprint) for fingerprint in ["a","b","c"] ]
+    actual = [ MockInternalEvent(fingerprint) for fingerprint
+               in ["a","d","d","d","d","b","d","d","d"] ]
+
+    result = match_fingerprints(actual, expected)
+    result = [ r.fingerprint for r in result ]
+    self.assertEqual(["a","d","d","d","d","b"], result)
+
+  def test_match_fingerprints_duplicate_expected(self):
+    expected = [ MockInternalEvent(fingerprint) for fingerprint
+                 in ["a","b","c","b","c","f"] ]
+    actual = [ MockInternalEvent(fingerprint) for fingerprint
+               in ["a","d","d","d","d","c","b","d","d","d","c","d"] ]
+
+    result = match_fingerprints(actual, expected)
+    result = [ r.fingerprint for r in result ]
+    self.assertEqual(["a","d","d","d","d","c","b","d","d","d","c"], result)
+
+  def test_match_fingerprints_duplicate_inferred(self):
+    expected = [ MockInternalEvent(fingerprint) for fingerprint
+                 in ["a","b","c"] ]
+    actual = [ MockInternalEvent(fingerprint) for fingerprint
+               in ["a","d","d","d","d","c","b","d","d","d","c","d"] ]
+
+    result = match_fingerprints(actual, expected)
+    result = [ r.fingerprint for r in result ]
+    # TODO(cs): perhaps we should include "b"? [i.e., make the inferrence
+    # unordered or semi-ordered]
+    self.assertEqual(["a","d","d","d","d","c"], result)
+
+  def test_match_fingerprints_empty(self):
+    expected = [ MockInternalEvent(fingerprint) for fingerprint
+                 in ["a","b","c"] ]
+    actual = [ MockInternalEvent(fingerprint) for fingerprint
+               in ["d","e","f"] ]
+
+    result = match_fingerprints(actual, expected)
+    result = [ r.fingerprint for r in result ]
+    # TODO(cs): perhaps we should include "b"? [i.e., make the inferrence
+    # unordered or semi-ordered]
+    self.assertEqual([], result)
+
 
 if __name__ == '__main__':
   unittest.main()
