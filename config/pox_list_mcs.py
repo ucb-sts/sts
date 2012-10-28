0c6d02364755ddf8a9c47d6fb13e9110bb79c8bb
colin-scott <cs@cs.berkeley.edu>
2012-10-28 10:45:14 -0700

    pox_list_mcs: check liveness, not correspondence

--- config/pox_list_mcs.py
+++ config/pox_list_mcs.py
@@ -2,11 +2,13 @@
 from experiment_config_lib import ControllerConfig
 from sts.topology import *
 from sts.control_flow import Replayer, MCSFinder
+from sts.invariant_checker import InvariantChecker
 
 controllers = [ControllerConfig(cmdline='./pox.py --verbose --no-cli openflow.of_01 --address=__address__ --port=__port__ sts.syncproto.pox_syncer samples.topo forwarding.l2_multi messenger.messenger samples.nommessenger', address='127.0.0.1', port=8888, cwd='pox', sync='tcp:localhost:18899')]
 topology_class = MeshTopology
 topology_params = "num_switches=2"
 patch_panel_class = BufferedPatchPanel
 control_flow = MCSFinder("input_traces/pox_list_violation.trace",
-                         wait_time=0.2, max_rounds=5)
+                         wait_time=0.2, max_rounds=5,
+                         invariant_check=InvariantChecker.check_liveness)
 dataplane_trace = "dataplane_traces/ping_pong_same_subnet.trace"
