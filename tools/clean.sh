54f776afd4f4eff042a0ae42816f60edfed31578
colin-scott <cs@cs.berkeley.edu>
2012-12-11 04:03:56 -0800

    Save .project if there is one

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Invoke with: ./tools/clean.sh
+
+if [ -f .project ]; then
+    cp .project .pydevproject /tmp
+fi
+
 git clean -fX
 cd sts/headerspace/hassel-c
 make
+
+if [ -f .project ]; then
+    cp /tmp/.project /tmp/.pydevproject .
+fi
