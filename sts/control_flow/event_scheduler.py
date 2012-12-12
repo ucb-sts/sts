c75f0949807dcce93bbf440807da918e896c88b7
colin-scott <cs@cs.berkeley.edu>
2012-12-12 15:08:50 -0800

    Change names of all variables to include units -- consolodate "timeout" and "epsilon" paramters.

--- sts/control_flow/event_scheduler.py
+++ sts/control_flow/event_scheduler.py
@@ -4,28 +4,25 @@ from sts.replay_event import *
 
 class DumbEventScheduler(object):
 
-  kwargs = set(['epsilon_ms', 'timeout_seconds', 'sleep_interval_seconds'])
+  kwargs = set(['epsilon_seconds', 'sleep_interval_seconds'])
 
-  def __init__(self, simulation, epsilon_seconds=0.0, timeout_seconds=1.0,
-               sleep_interval_seconds=0.2):
+  def __init__(self, simulation, epsilon_seconds=0.0, sleep_interval_seconds=0.2):
     self.simulation = simulation
     self.epsilon_seconds = epsilon_seconds
-    self.timeout_seconds = timeout_seconds
     self.sleep_interval_seconds = sleep_interval_seconds
     self.last_event = None
 
   def schedule(self, event):
     if self.last_event:
-      rec_delta = (event.time.as_float() - self.last_event.time.as_float()) + self.epsilon_seconds
+      rec_delta = (event.time.as_float() - self.last_event.time.as_float())
       if rec_delta > 0:
         log.info("Sleeping for %.0f ms before next event" % (rec_delta * 1000))
         self.simulation.io_master.sleep(rec_delta)
 
     start = time.time()
-    end = start + self.timeout_seconds - 0.01
 
     log.debug("Waiting for %r (maximum wait time: %.0f ms)" %
-          ( event, self.timeout_seconds * 1000) )
+          ( event, self.epsilon_seconds * 1000) )
 
     proceed = False
     while True:
@@ -46,17 +43,20 @@ class EventScheduler(object):
   '''an EventWatchers schedules events. It controls their admission, and
   any post-event delay '''
 
-  kwargs = set(['timeout' 'speedup', 'delay_input_events', 'initial_wait', 'wait_delta'])
+  kwargs = set(['speedup', 'delay_input_events', 'initial_wait',
+                'epsilon_seconds', 'sleep_interval_seconds'])
 
-  def __init__(self, simulation, timeout=0.5, speedup=1.0, delay_input_events=True, initial_wait=0.5, wait_delta=0):
+  def __init__(self, simulation, speedup=1.0,
+               delay_input_events=True, initial_wait=0.5, epsilon_seconds=0.5,
+               sleep_interval_seconds=0.2):
     self.simulation = simulation
-    self.timeout = timeout
     self.speedup = speedup
     self.delay_input_events = delay_input_events
     self.last_real_time = None
     self.last_rec_time = None
     self.initial_wait = initial_wait
-    self.wait_delta = wait_delta
+    self.epsilon_seconds = epsilon_seconds
+    self.sleep_interval_seconds = sleep_interval_seconds
 
   def schedule(self, event):
     if isinstance(event, InputEvent):
@@ -67,35 +67,38 @@ class EventScheduler(object):
 
   def inject_input(self, event):
     if self.delay_input_events:
-      wait_time = self.wait_time(event)
-      if wait_time > 0.01:
+      wait_time_seconds = self.wait_time(event)
+      if wait_time_seconds > 0.01:
         log.debug("Delaying input_event %r for %.0f ms" %
-            ( event , (wait_time) * 1000 ))
+            ( event , (wait_time_seconds) * 1000 ))
 
-        self.simulation.io_master.sleep(wait_time)
+        self.simulation.io_master.sleep(wait_time_seconds)
     log.debug("Injecting %r", event)
-    end = event.time.as_float() + self.timeout
-    self._poll_event(event, self.timeout, end)
+    # TODO(cs): AFACT, this is essentially a dummy variable? Since event.time
+    # is in the past... Andi, can you verify this?
+    end = event.time.as_float()
+    self._poll_event(event, end)
 
   def wait_for_internal(self, event):
-    timeout = self.wait_time(event) + self.timeout
+    wait_time_seconds = self.wait_time(event)
     start = time.time()
-    end = start + timeout - 0.01
+    # TODO(cs): why - 0.01?
+    end = start + wait_time_seconds - 0.01 + self.epsilon_seconds
 
     log.debug("Waiting for %r (maximum wait time: %.0f ms)" %
-          ( event, timeout * 1000) )
-    self._poll_event(event, timeout, end)
+          ( event, wait_time_seconds * 1000) )
+    self._poll_event(event, end)
 
-  def _poll_event(self, event, timeout, end):
+  def _poll_event(self, event, end_time):
     proceed = False
     while True:
       now = time.time()
       if event.proceed(self.simulation):
         proceed = True
         break
-      elif now > end:
+      elif now > end_time:
         break
-      self.simulation.io_master.select(timeout)
+      self.simulation.io_master.select(self.sleep_interval_seconds)
     if proceed:
       log.debug("Succcessfully executed %r" % event)
       self.update_event_time(event)
@@ -108,15 +111,15 @@ class EventScheduler(object):
     self.last_rec_time = event.time
 
   def wait_time(self, event):
-    """ returns how long to wait for an event to occur or be injected. """
+    """ returns how long to wait in seconds for an event to occur or be injected. """
     if not self.last_real_time:
       return self.initial_wait
 
-    rec_delta = (event.time.as_float() - self.last_rec_time.as_float()) / self.speedup + self.wait_delta
+    rec_delta = (event.time.as_float() - self.last_rec_time.as_float()) / self.speedup
     real_delta = time.time() - self.last_real_time
 
     to_wait = rec_delta - real_delta
     if to_wait > 10000:
-      raise RuntimeError("to_wait %d ms is way too big for event" %
+      raise RuntimeError("to_wait %d ms is way too big for event %s" %
                          (to_wait, str(event)))
     return max(to_wait, 0)
