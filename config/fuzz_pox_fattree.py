5c90f4c131b5a97c98d1f20873d2f8dc75baf6d3
colin-scott <cs@cs.berkeley.edu>
2012-12-11 19:59:14 -0800

    Halt on failure in fuzz_pox_fattree

--- config/fuzz_pox_fattree.py
+++ config/fuzz_pox_fattree.py
@@ -2,6 +2,7 @@ from experiment_config_lib import ControllerConfig
 from sts.control_flow import Fuzzer
 from sts.input_traces.input_logger import InputLogger
 from sts.simulation_state import SimulationConfig
+from sts.invariant_checker import InvariantChecker
 
 # Use POX as our controller
 command_line = "./pox.py --verbose --no-cli openflow.of_01 --address=__address__ --port=__port__ sts.syncproto.pox_syncer samples.topo forwarding.l2_multi messenger.messenger samples.nommessenger"
@@ -13,5 +14,7 @@ simulation_config = SimulationConfig(controller_configs=controllers,
                                      dataplane_trace=dataplane_trace)
 
 # Use a Fuzzer (already the default)
-control_flow = Fuzzer(simulation_config, check_interval=1, input_logger=InputLogger())
+control_flow = Fuzzer(simulation_config, check_interval=1, halt_on_violation=True,
+                      input_logger=InputLogger(),
+                      invariant_check=InvariantChecker.check_liveness)
 
