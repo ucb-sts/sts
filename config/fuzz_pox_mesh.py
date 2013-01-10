939319ddece1925c8c3152f4437b4848749b85b3
colin-scott <cs@cs.berkeley.edu>
2013-01-09 22:04:49 -0800

    Add a config that exercises the Multiplexed socketS

--- config/fuzz_pox_mesh.py
+++ config/fuzz_pox_mesh.py
@@ -6,7 +6,10 @@ from sts.invariant_checker import InvariantChecker
 from sts.simulation_state import SimulationConfig
 
 # Use POX as our controller
-command_line = "./pox.py --verbose --no-cli openflow.of_01 --address=../sts_socket_pipe sts.syncproto.pox_syncer samples.topo forwarding.l2_multi messenger.messenger samples.nommessenger"
+command_line = ('''./pox.py --verbose --no-cli sts.syncproto.pox_syncer '''
+                '''samples.topo forwarding.l2_multi '''
+                '''sts.util.socket_mux.pox_monkeypatcher '''
+                '''openflow.of_01 --address=../sts_socket_pipe''')
 controllers = [ControllerConfig(command_line, address="sts_socket_pipe", cwd="pox", sync="tcp:localhost:18899")]
 
 topology_class = MeshTopology
@@ -16,7 +19,8 @@ dataplane_trace = "dataplane_traces/ping_pong_same_subnet.trace"
 simulation_config = SimulationConfig(controller_configs=controllers,
                                      topology_class=topology_class,
                                      topology_params=topology_params,
-                                     dataplane_trace=dataplane_trace)
+                                     dataplane_trace=dataplane_trace,
+                                     monkey_patch_select=True)
 
 control_flow = Fuzzer(simulation_config, check_interval=1, halt_on_violation=True,
                       input_logger=InputLogger(),
