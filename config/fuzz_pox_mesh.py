fc076d4b390c8e28fceb9613b04423ad374930e5
colin-scott <cs@cs.berkeley.edu>
2013-01-20 12:03:33 -0800

    Switch to normal pox instead of betta

--- config/fuzz_pox_mesh.py
+++ config/fuzz_pox_mesh.py
@@ -6,12 +6,11 @@ from sts.invariant_checker import InvariantChecker
 from sts.simulation_state import SimulationConfig
 
 # Use POX as our controller
-command_line = ('''./pox.py --verbose openflow.debug ''' #sts.syncproto.pox_syncer '''
-                '''forwarding.l2_multi '''
-                #'''sts.util.socket_mux.pox_monkeypatcher '''
+command_line = ('''./pox.py --verbose sts.syncproto.pox_syncer '''
+                '''openflow.discovery forwarding.l2_multi '''
+                '''sts.util.socket_mux.pox_monkeypatcher '''
                 '''openflow.of_01 --address=__address__ --port=__port__''')
-controllers = [ControllerConfig(command_line, cwd="betta")]
-
+controllers = [ControllerConfig(command_line, cwd="pox", sync="tcp:localhost:18899")]
 topology_class = MeshTopology
 topology_params = "num_switches=2"
 dataplane_trace = "dataplane_traces/ping_pong_same_subnet.trace"
@@ -20,10 +19,10 @@ simulation_config = SimulationConfig(controller_configs=controllers,
                                      topology_class=topology_class,
                                      topology_params=topology_params,
                                      dataplane_trace=dataplane_trace,
-                                     multiplex_sockets=False)
+                                     multiplex_sockets=True)
 
 control_flow = Fuzzer(simulation_config, check_interval=80,
                       halt_on_violation=False,
                       input_logger=InputLogger(),
-                      invariant_check=InvariantChecker.check_connectivity)
+                      invariant_check=InvariantChecker.check_loops)
 #control_flow = Interactive(simulation_config, input_logger=InputLogger())
