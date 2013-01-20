1b2f0be67a8372a652b786c8b183cd5edf1807cd
colin-scott <cs@cs.berkeley.edu>
2013-01-19 17:26:35 -0800

    Swap back to Fuzzer, no monkey patching

--- config/fuzz_pox_mesh.py
+++ config/fuzz_pox_mesh.py
@@ -1,27 +1,29 @@
 from experiment_config_lib import ControllerConfig
 from sts.topology import MeshTopology
-from sts.control_flow import Fuzzer
+from sts.control_flow import Fuzzer, Interactive
 from sts.input_traces.input_logger import InputLogger
 from sts.invariant_checker import InvariantChecker
 from sts.simulation_state import SimulationConfig
 
 # Use POX as our controller
-command_line = ('''./pox.py --verbose --no-cli sts.syncproto.pox_syncer '''
-                '''samples.topo forwarding.l2_multi '''
-                '''sts.util.socket_mux.pox_monkeypatcher '''
-                '''openflow.of_01 --address=../sts_socket_pipe''')
-controllers = [ControllerConfig(command_line, address="sts_socket_pipe", cwd="pox", sync="tcp:localhost:18899")]
+command_line = ('''./pox.py --verbose openflow.debug ''' #sts.syncproto.pox_syncer '''
+                '''forwarding.l2_multi '''
+                #'''sts.util.socket_mux.pox_monkeypatcher '''
+                '''openflow.of_01 --address=__address__ --port=__port__''')
+controllers = [ControllerConfig(command_line, cwd="betta")]
 
 topology_class = MeshTopology
-topology_params = "num_switches=4"
-dataplane_trace = "dataplane_traces/ping_pong_same_subnet_4_switches.trace"
+topology_params = "num_switches=2"
+dataplane_trace = "dataplane_traces/ping_pong_same_subnet.trace"
 
 simulation_config = SimulationConfig(controller_configs=controllers,
                                      topology_class=topology_class,
                                      topology_params=topology_params,
                                      dataplane_trace=dataplane_trace,
-                                     multiplex_sockets=True)
+                                     multiplex_sockets=False)
 
-control_flow = Fuzzer(simulation_config, check_interval=1, halt_on_violation=True,
+control_flow = Fuzzer(simulation_config, check_interval=80,
+                      halt_on_violation=False,
                       input_logger=InputLogger(),
-                      invariant_check=InvariantChecker.check_liveness)
+                      invariant_check=InvariantChecker.check_connectivity)
+#control_flow = Interactive(simulation_config, input_logger=InputLogger())
