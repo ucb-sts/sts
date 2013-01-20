cde1be5f4f8b3effe2d6802db368167a35495878
colin-scott <cs@cs.berkeley.edu>
2013-01-19 20:32:24 -0800

    Add a tool to convert pcap to Murphy's python classes.
    Not quite useful yet

---
+++ tools/load_pcap.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Installed from: http://dirtbags.net/py-pcap.html
+import pcap
+import argparse
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "pox"))
+from pox.lib.packet.ethernet import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input')
+parser.add_argument('-o', '--output', default="pcap.trace")
+
+args = parser.parse_args()
+
+p = pcap.open(args.input)
+for i in p:
+  e = ethernet(raw=i[1])
+  import pdb; pdb.set_trace()
+  print e
