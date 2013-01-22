d83cbe7c2cc66464b1cc98479c76cca56aa963a5
Andreas Wundsam <b-github.com@wundsam.net>
2013-01-21 19:06:49 -0800

    add funky invariant checker :)

--- config/fuzz_pox_mesh.py
+++ config/fuzz_pox_mesh.py
@@ -6,11 +6,16 @@ from sts.invariant_checker import InvariantChecker
 from sts.simulation_state import SimulationConfig
 
 # Use POX as our controller
-command_line = ('''./pox.py --verbose sts.syncproto.pox_syncer '''
+command_line = ('''./pox.py --verbose --random-seed=1 '''
+                #'''sts.syncproto.pox_syncer --blocking=False '''
+                #'''openflow.mock_discovery forwarding.l2_multi '''
                 '''openflow.discovery forwarding.l2_multi '''
-                '''sts.util.socket_mux.pox_monkeypatcher '''
+                #'''sts.util.socket_mux.pox_monkeypatcher '''
                 '''openflow.of_01 --address=__address__ --port=__port__''')
-controllers = [ControllerConfig(command_line, cwd="pox", sync="tcp:localhost:18899")]
+controllers = [ControllerConfig(command_line, cwd="betta",
+  #sync="tcp:localhost:18899"
+  )
+  ]
 topology_class = MeshTopology
 topology_params = "num_switches=2"
 dataplane_trace = "dataplane_traces/ping_pong_same_subnet.trace"
@@ -19,13 +24,29 @@ simulation_config = SimulationConfig(controller_configs=controllers,
                                      topology_class=topology_class,
                                      topology_params=topology_params,
                                      dataplane_trace=dataplane_trace,
-                                     multiplex_sockets=True)
+                                     #multiplex_sockets=True
+                                     )
 
-control_flow = Fuzzer(simulation_config, check_interval=80,
+def my_funky_invariant_check(simulation):
+  from sts.invariant_checker import InvariantChecker
+  result = InvariantChecker.check_loops(simulation)
+  if result:
+    return result
+  result = InvariantChecker.check_connectivity(simulation)
+  if not result:
+    print "Connectivity established - bailing out"
+    import sys
+    sys.exit(0)
+  return []
+
+
+
+control_flow = Fuzzer(simulation_config, check_interval=20,
+                      #mock_link_discovery=True,
                       halt_on_violation=True,
                       input_logger=InputLogger(),
-                      invariant_check=InvariantChecker.check_loops,
-                      steps=241,
-                      random_seed=1738290519,
+                      invariant_check=my_funky_invariant_check,
+                      steps=141,
+                      #random_seed=466448715
                       )
 #control_flow = Interactive(simulation_config, input_logger=InputLogger())
