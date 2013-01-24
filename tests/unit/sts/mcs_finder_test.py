53cb9a2647aaf4cb4632f57f1f3473a14c325377
colin-scott <cs@cs.berkeley.edu>
2013-01-23 18:34:25 -0800

    Forgot to commit new mcs_finder_test

---
+++ tests/unit/sts/mcs_finder_test.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+import unittest
+import sys
+import os
+import itertools
+from copy import copy
+import types
+import signal
+import tempfile
+
+from config.experiment_config_lib import ControllerConfig
+from sts.control_flow import Replayer, MCSFinder, EfficientMCSFinder
+from sts.topology import FatTree, PatchPanel, MeshTopology
+from sts.simulation_state import Simulation, SimulationConfig
+from sts.replay_event import Event, InternalEvent, InputEvent
+from sts.event_dag import EventDag
+from sts.entities import Host, Controller
+import logging
+
+sys.path.append(os.path.dirname(__file__) + "/../../..")
+
+class MockMCSFinderBase(object):
+  ''' Overrides self.invariant_check and run_simulation_forward() '''
+  def __init__(self, event_dag, mcs):
+    self.dag = event_dag
+    self.new_dag = None
+    self.mcs = mcs
+    self.mcs_trace_path = None
+    self.transform_dag = None
+    self.simulation_cfg = None
+    self.ignore_powersets = False
+    self.no_violation_verification_runs = True
+    self._extra_log = None
+    self._runtime_stats = {}
+
+  def log(self, message):
+    self._log.info(message)
+
+  def invariant_check(self, _):
+    for e in self.mcs:
+      if e not in self.new_dag._events_set:
+        return []
+    return ["violation"]
+
+  def replay(self, new_dag, hook=None):
+    self.new_dag = new_dag
+    return self.invariant_check(new_dag)
+
+# Horrible horrible hack. This way lies insanity
+class MockMCSFinder(MockMCSFinderBase, MCSFinder):
+  def __init__(self, event_dag, mcs):
+    MockMCSFinderBase.__init__(self, event_dag, mcs)
+    self._log = logging.getLogger("mock_mcs_finder")
+
+class MockEfficientMCSFinder(MockMCSFinderBase, EfficientMCSFinder):
+  def __init__(self, event_dag, mcs):
+    MockMCSFinderBase.__init__(self, event_dag, mcs)
+    self._log = logging.getLogger("mock_efficient_mcs_finder")
+
+class MockInputEvent(InputEvent):
+  def __init__(self, fingerprint=None, **kws):
+    super(MockInputEvent, self).__init__(**kws)
+    self.fingerprint = fingerprint
+
+  def proceed(self, simulation):
+    return True
+
+class MCSFinderTest(unittest.TestCase):
+  def test_basic(self):
+    self.basic(MockMCSFinder)
+
+  def test_basic_efficient(self):
+    self.basic(MockEfficientMCSFinder)
+
+  def basic(self, mcs_finder_type):
+    trace = [ MockInputEvent(fingerprint=("class",f)) for f in range(1,7) ]
+    dag = EventDag(trace)
+    mcs = [trace[0]]
+    mcs_finder = mcs_finder_type(dag, mcs)
+    result = mcs_finder.simulate()
+    self.assertEqual(mcs, result)
+
+  def test_straddle(self):
+    self.straddle(MockMCSFinder)
+
+  def test_straddle_efficient(self):
+    self.straddle(MockEfficientMCSFinder)
+
+  def straddle(self, mcs_finder_type):
+    trace = [ MockInputEvent(fingerprint=("class",f)) for f in range(1,7) ]
+    dag = EventDag(trace)
+    mcs = [trace[0],trace[5]]
+    mcs_finder = mcs_finder_type(dag, mcs)
+    result = mcs_finder.simulate()
+    self.assertEqual(mcs, result)
+
+  def test_all(self):
+    self.all(MockMCSFinder)
+
+  def test_all_efficient(self):
+    self.all(MockEfficientMCSFinder)
+
+  def all(self, mcs_finder_type):
+    trace = [ MockInputEvent(fingerprint=("class",f)) for f in range(1,7) ]
+    dag = EventDag(trace)
+    mcs = trace
+    mcs_finder = mcs_finder_type(dag, mcs)
+    result = mcs_finder.simulate()
+    self.assertEqual(mcs, result)
+
+if __name__ == '__main__':
+  unittest.main()
