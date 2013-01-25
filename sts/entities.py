a442a55ba6436d4b8da3f80b3c6318c10ce0c026
colin-scott <cs@cs.berkeley.edu>
2013-01-25 15:24:05 -0800

    Should be an exception rather than a WARN

--- sts/entities.py
+++ sts/entities.py
@@ -115,8 +115,7 @@ class FuzzSoftwareSwitch (NXSoftwareSwitch):
       self.log.warn("Switch already up")
       return
     if self.create_connection is None:
-      self.log.warn("Never connected in the first place")
-      return False
+      raise RuntimeError("Never connected in the first place")
 
     connected_to_at_least_one = self.connect(self.create_connection,
                                              down_controller_ids=down_controller_ids)
