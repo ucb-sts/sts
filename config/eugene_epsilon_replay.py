e93a2946b2f1f19ab340fe263ad6ca3bb2189cb9
colin-scott <cs@cs.berkeley.edu>
2013-01-25 13:16:41 -0800

    Add config files for Eugunge traces

---
+++ config/eugene_epsilon_replay.py
@@ -0,0 +1,16 @@
+
+from experiment_config_lib import ControllerConfig
+from sts.topology import *
+from sts.control_flow import Replayer
+from sts.simulation_state import SimulationConfig
+
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='./pox.py --verbose --no-cli openflow.of_01 --address=__address__ --port=__port__ sts.syncproto.pox_syncer samples.topo forwarding.l2_multi messenger.messenger samples.nommessenger', address='127.0.0.1', port=6635, cwd='pox', sync='tcp:localhost:18899')],
+                 topology_class=MeshTopology,
+                 topology_params="num_switches=4",
+                 patch_panel_class=BufferedPatchPanel,
+                 dataplane_trace="dataplane_traces/ping_pong_same_subnet_4_switches.trace",
+                 multiplex_sockets=False)
+
+control_flow = Replayer(simulation_config, "input_traces/eugene_epsilon.trace",
+                        # MCS trace path: input_traces/2013_01_24_13_03_21_mcs_final.trace
+                        wait_on_deterministic_values=False)
