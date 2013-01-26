1f4a306720c91602711c29dd667dafb312ac2708
colin-scott <cs@cs.berkeley.edu>
2013-01-26 12:13:56 -0800

    Update Eugene's config files

--- config/eugene_epsilon_mcs.py
+++ config/eugene_epsilon_mcs.py
@@ -5,7 +5,7 @@ from sts.control_flow import EfficientMCSFinder
 from sts.invariant_checker import InvariantChecker
 from sts.simulation_state import SimulationConfig
 
-simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='./pox.py --verbose --no-cli openflow.of_01 --address=__address__ --port=__port__ sts.syncproto.pox_syncer samples.topo forwarding.l2_multi messenger.messenger samples.nommessenger', address='127.0.0.1', port=6635, cwd='pox', sync='tcp:localhost:18899')],
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='./pox.py --verbose --no-cli openflow.of_01 --address=__address__ --port=__port__ sts.syncproto.pox_syncer samples.topo forwarding.l2_multi messenger.messenger samples.nommessenger', address='127.0.0.1', port=6633, cwd='pox', sync='tcp:localhost:18899')],
                  topology_class=MeshTopology,
                  topology_params="num_switches=4",
                  dataplane_trace="dataplane_traces/ping_pong_same_subnet_4_switches.trace")
--- config/eugene_epsilon_replay.py
+++ config/eugene_epsilon_replay.py
@@ -4,7 +4,7 @@ from sts.topology import *
 from sts.control_flow import Replayer
 from sts.simulation_state import SimulationConfig
 
-simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='./pox.py --verbose --no-cli openflow.of_01 --address=__address__ --port=__port__ sts.syncproto.pox_syncer samples.topo forwarding.l2_multi messenger.messenger samples.nommessenger', address='127.0.0.1', port=6635, cwd='pox', sync='tcp:localhost:18899')],
+simulation_config = SimulationConfig(controller_configs=[ControllerConfig(cmdline='./pox.py --verbose --no-cli openflow.of_01 --address=__address__ --port=__port__ sts.syncproto.pox_syncer samples.topo forwarding.l2_multi messenger.messenger samples.nommessenger', address='127.0.0.1', port=6633, cwd='pox', sync='tcp:localhost:18899')],
                  topology_class=MeshTopology,
                  topology_params="num_switches=4",
                  patch_panel_class=BufferedPatchPanel,
