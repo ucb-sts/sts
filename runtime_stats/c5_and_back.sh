3b6eb4d64220ca72e9344a130b618977bb2a0a5b
colin-scott <cs@cs.berkeley.edu>
2013-01-25 17:39:33 -0800

    remove the directory every time

--- runtime_stats/c5_and_back.sh
+++ runtime_stats/c5_and_back.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 user=`whoami`
-ssh $user@c5.millennium.berkeley.edu mkdir -p /tmp/graphs
+if [ $user == "cs" ]; then
+  user="rcs"
+fi
+ssh $user@c5.millennium.berkeley.edu "rm -rf /tmp/graphs/* && mkdir -p /tmp/graphs"
 scp *gpi *dat $user@c5.millennium.berkeley.edu:/tmp/graphs
 ssh $user@c5.millennium.berkeley.edu "cd /tmp/graphs && for F in *gpi; do gnuplot \$F; done"
 scp $user@c5.millennium.berkeley.edu:/tmp/graphs/*pdf .
