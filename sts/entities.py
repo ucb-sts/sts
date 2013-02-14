fd9920fc1ff1692adbf4d328e5f857e1f67b8612
Sam Whitlock <phynominal@gmail.com>
2013-02-13 17:46:36 -0800

    copying from old netns branch

--- sts/entities.py
+++ sts/entities.py
@@ -11,11 +11,22 @@ from pox.lib.revent import EventMixin
 from sts.util.procutils import popen_filtered, kill_procs
 from sts.util.console import msg
 from itertools import count
+from pox.lib.addresses import EthAddr
 
 import logging
 import os
+import socket
+import subprocess
+import fcntl
+import struct
 import re
 import pickle
+import sys
+
+from pox.lib.addresses import EthAddr
+from os import geteuid
+from exceptions import EnvironmentError
+from platform import system
 
 class DeferredOFConnection(OFConnection):
   def __init__(self, io_worker, cid, dpid, god_scheduler):
@@ -312,6 +323,92 @@ class Host (EventMixin):
 
   def __repr__(self):
     return "Host(%d)" % self.hid
+
+class NamespaceHost(Host):
+  '''
+  A host that launches a process in a separate namespace process.
+  '''
+
+  def __init__(self, interfaces, name="", cmd="xterm"):
+    '''
+    - cmd: a string of the command to execute in the separate namespace
+      The default is "xterm".
+    '''
+    super(NamespaceHost, self).__init__(interfaces, name)
+    self._launch_namespace(cmd)
+
+  def _launch_namespace(cmd):
+    '''
+    - cmd: the string to launch, in a separate namespace
+    '''
+    '''
+  Set up and launch cmd in a new network namespace.
+
+  Returns a tuple of the (socket, Popen object of unshared project in netns, EthAddr of guest device).
+
+  This method uses functionality that requires CAP_NET_ADMIN capabilites. This
+  means that the calling method should check that the python process was
+  launched as admin/superuser.
+  '''
+
+  if system() != 'Linux':
+    raise EnvironmentError('network namespace functionality requires a Linux environment')
+
+  uid = geteuid()
+  if uid != 0:
+    # user must have CAP_NET_ADMIN, which doesn't have to be su, but most often is
+    raise EnvironmentError("superuser privileges required to launch network namespace")
+
+  iface_index = self.hid
+
+  host_device = "heth%d" % (iface_index)
+  guest_device = "geth%d" % (iface_index)
+
+  try:
+    null = open(os.devnull, 'wb') # FIXME(sw): this file is never actually closed
+
+    for dev in (host_device, guest_device):
+      if subprocess.call(['ip', 'link', 'show', dev], stdout=null, stderr=sys.stderr) == 0:
+        # Delete the device if it already exists in case cleanup was bad previously
+        subprocess.check_call(['ip', 'link', 'del', dev])
+
+    # create a veth pair and set the host end to be promiscuous
+    subprocess.check_call(['ip','link','add','name',host_device,'type','veth','peer','name',guest_device])
+    subprocess.check_call(['ip','link','set',host_device,'promisc','on'])
+    subprocess.check_call(['ip','link','set',host_device,'up'])
+  except subprocess.CalledProcessError:
+    raise # TODO raise a more informative exception
+
+  guest_eth_addr = get_eth_address_for_interface(guest_device)
+
+  # make the host-side socket
+  # do this before unshare/fork to make failure/cleanup easier
+  s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, ETH_P_ALL)
+  s.bind((host_device, ETH_P_ALL))
+  s.setblocking(0) # set non-blocking
+
+  # all else should have succeeded, so now we fork and unshare for the guest
+  guest = subprocess.Popen(["unshare", "-n", "--"] + cmd.split())
+
+  # push down the guest device into the netns
+  try:
+    subprocess.check_call(['ip', 'link', 'set', guest_device, 'netns', str(guest.pid)])
+  except subprocess.CalledProcessError:
+    # Failed to push down guest side of veth pair
+    s.close()
+    raise # TODO raise a more informative exception
+
+  # TODO(sw): save relevant state (s, guest, guest_eth_addr, guest_device)
+
+  @staticmethod
+  def get_eth_address_for_interface(ifname):
+    '''Returns an EthAddr object from the interface specified by the argument.
+
+    interface is a string, commonly eth0, wlan0, lo.'''
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
+    return EthAddr(''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1])
+
 
 class Controller(object):
   '''Encapsulates the state of a running controller.'''
