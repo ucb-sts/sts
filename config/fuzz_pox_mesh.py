86e6ba151776dbbd30379467914bbcae7b207b4c
colin-scott <cs@cs.berkeley.edu>
2013-04-09 18:29:00 -0700

    Don't use topology component by default

--- config/fuzz_pox_mesh.py
+++ config/fuzz_pox_mesh.py
@@ -8,7 +8,7 @@ from sts.simulation_state import SimulationConfig
 # Use POX as our controller
 command_line = ('''./pox.py --verbose '''
                 '''sts.syncproto.pox_syncer --blocking=False '''
-                '''samples.topo forwarding.l2_multi '''
+                '''openflow.discovery forwarding.l2_multi '''
                 '''sts.util.socket_mux.pox_monkeypatcher '''
                 '''openflow.of_01 --address=__address__ --port=__port__''')
 controllers = [ControllerConfig(command_line, cwd="pox", sync="tcp:localhost:18899")]
