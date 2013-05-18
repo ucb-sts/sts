455e9ab39846b55ca90408d224347ae306219b7e
colin-scott <cs@cs.berkeley.edu>
2013-05-17 18:26:51 -0700

    Fix terrible capitalization commit

--- tools/pretty_print_input_trace.py
+++ tools/pretty_print_input_trace.py
@@ -11,21 +11,21 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 import sts.replay_event as replay_events
-from sts.dataplane_traces.trace import trace
+from sts.dataplane_traces.trace import Trace
 from sts.input_traces.log_parser import parse
 
-parser = argparse.argumentparser()
-parser.add_argument('input', metavar="input",
-                    help='the input json file to be printed')
+parser = argparse.ArgumentParser()
+parser.add_argument('input', metavar="INPUT",
+                    help='The input json file to be printed')
 parser.add_argument('-f', '--format-file',
                     help='the output format configuration file',
-                    default=none)
+                    default=None)
 parser.add_argument('-n', '--no-stats', action="store_false", dest="stats",
                     help="don't print statistics",
-                    default=true)
+                    default=True)
 parser.add_argument('-d', '--dp-trace-path', dest="dp_trace_path",
-                    help="for older traces, specify path to the trafficinjection packets",
-                    default=none)
+                    help="for older traces, specify path to the TrafficInjection packets",
+                    default=None)
 args = parser.parse_args()
 
 # ------------------------------- config file format: --------------------------------------
@@ -51,11 +51,11 @@ def round_printer(event):
   print "round: %d" % event.round
 
 def fingerprint_printer(event):
-  fingerprint = none
+  fingerprint = None
   if hasattr(event, 'fingerprint'):
     # the first element of the fingerprint tuple is always the class name, so
     # we skip it over
-    # todo(cs): make sure that dict fields are always in the same order
+    # TODO(cs): make sure that dict fields are always in the same order
     fingerprint = event.fingerprint[1:]
   print "fingerprint: ", fingerprint
 
@@ -65,7 +65,7 @@ def _timestamp_to_string(timestamp):
   epoch = float(sec) + float(micro_sec) / 1e6
   struct_time = time.localtime(epoch)
   # hour:minute:second
-  no_micro = time.strftime("%x", struct_time)
+  no_micro = time.strftime("%X", struct_time)
   # hour:minute:second:microsecond
   with_micro = no_micro + ":%d" % micro_sec
   return with_micro
@@ -82,16 +82,16 @@ field_formatters = {
   'fingerprint' : fingerprint_printer,
   'event_delimiter' : event_delim_printer,
   'abs_time' : abs_time_printer,
-  # todo(cs): allow user to display relative time between events
+  # TODO(cs): allow user to display relative time between events
 }
 
-class stats:
+class Stats:
   def __init__(self):
     self.input_events = {}
     self.internal_events = {}
 
   def update(self, event):
-    if isinstance(event, replay_events.inputevent):
+    if isinstance(event, replay_events.InputEvent):
       event_name = str(event.__class__.__name__)
       if event_name in self.input_events.keys():
 	      self.input_events[event_name] += 1
@@ -123,13 +123,13 @@ class stats:
     return self.input_event_count + self.internal_event_count
 
   def __str__(self):
-    s = "events: %d total (%d input, %d internal).\n" % (self.total_event_count, self.input_event_count, self.internal_event_count)
+    s = "Events: %d total (%d input, %d internal).\n" % (self.total_event_count, self.input_event_count, self.internal_event_count)
     if len(self.input_events) > 0:
-      s += "\n\tinput events:\n"
+      s += "\n\tInput events:\n"
       for event_name, count in self.input_events.items():
         s += "\t  %s : %d\n" % (event_name, count)
     if len(self.internal_events) > 0:
-      s += "\n\tinternal events:\n"
+      s += "\n\tInternal events:\n"
       for event_name, count in self.internal_events.items():
         s += "\t  %s : %d\n" % (event_name, count)
     return s
@@ -141,14 +141,14 @@ def main(args):
     config = __import__(format_file, globals(), locals(), ["*"])
     return config
 
-  if args.format_file is not none:
+  if args.format_file is not None:
     format_def = load_format_file(args.format_file)
   else:
     format_def = object()
 
-  dp_trace = none
-  if args.dp_trace_path is not none:
-    dp_trace = trace(args.dp_trace_path).dataplane_trace
+  dp_trace = None
+  if args.dp_trace_path is not None:
+    dp_trace = Trace(args.dp_trace_path).dataplane_trace
 
   if hasattr(format_def, "fields"):
     fields = format_def.fields
@@ -157,14 +157,14 @@ def main(args):
 
   for field in fields:
     if field not in field_formatters:
-      raise valueerror("unknown field %s" % field)
+      raise ValueError("unknown field %s" % field)
 
   if hasattr(format_def, "filtered_classes"):
     filtered_classes = format_def.filtered_classes
   else:
     filtered_classes = default_filtered_classes
 
-  stats = stats()
+  stats = Stats()
 
   # all events are printed with a fixed number of lines, and (optionally)
   # separated by delimiter lines of the form:
@@ -173,14 +173,14 @@ def main(args):
     trace = parse(input_file)
     for event in trace:
       if type(event) not in filtered_classes:
-        if dp_trace is not none and type(event) == replay_events.trafficinjection:
+        if dp_trace is not None and type(event) == replay_events.TrafficInjection:
           event.dp_event = dp_trace.pop(0)
         for field in fields:
           field_formatters[field](event)
         stats.update(event)
 
   if args.stats:
-    print "stats: %s" % stats
+    print "Stats: %s" % stats
 
 if __name__ == '__main__':
   main(args)
