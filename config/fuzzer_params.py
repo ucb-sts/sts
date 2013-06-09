c4a6dbd3357a584fcf114aa89689f56ceab2ca79
colin-scott <cs@cs.berkeley.edu>
2013-06-09 16:00:51 -0700

    Tweak fuzzer params

--- config/fuzzer_params.py
+++ config/fuzzer_params.py
@@ -1,12 +1,12 @@
-switch_failure_rate = 0.05
-switch_recovery_rate = 0.05
-dataplane_drop_rate = 0.1
+switch_failure_rate = 0.0
+switch_recovery_rate = 0.0
+dataplane_drop_rate = 0.3
 controlplane_block_rate = 0.0
 controlplane_unblock_rate = 1.0
 ofp_message_receipt_rate = 1.0
 ofp_message_send_rate = 1.0
-link_failure_rate = 0.05
-link_recovery_rate = 0.2
+link_failure_rate = 0.0
+link_recovery_rate = 1.0
 controller_crash_rate = 0.0
 controller_recovery_rate = 0.0
 traffic_generation_rate = 0.3
