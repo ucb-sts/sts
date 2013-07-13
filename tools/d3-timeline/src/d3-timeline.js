5c909adf47c0ae039c3af7ede422cbd1fe9da973
colin-scott <cs@cs.berkeley.edu>
2013-07-12 20:20:47 -0700

    Move Chih's new filenames back to their original filenames
    (So that we can see the diffs)

--- tools/d3-timeline/src/d3-timeline.js
+++ tools/d3-timeline/src/d3-timeline.js
@@ -1,6 +1,7 @@
 (function() {
 
-  d3.timeline = function() {
+  // timeline_type currently only supports two types of timelines: 1d and 2d
+  d3.timeline = function(timeline_type) {
     var DISPLAY_TYPES = ["circle", "rect"];
 
     var hover = function () {},
@@ -19,13 +20,28 @@
         display = "rect",
         beginning = 0,
         ending = 0,
-        margin = {left: 30, right:400, top: 30, bottom:30},
+        margin = {left: 30, right:400, top: 30, bottom:1000},
         stacked = false,
         rotateTicks = false,
         itemHeight = 20,
         itemMargin = 5,
-        timelineDiffHeight = 150
-      ;
+        timelineDiffHeight = 150;
+
+    var eventColors = {"ConnectToControllers": colorCycle(0),
+                      "ControllerStateChange": colorCycle(1), 
+                      "ControlMessageSend": colorCycle(2),
+                      "ControlMessageReceive": colorCycle(3),
+                      "DataplaneDrop": colorCycle(4),
+                      "TrafficInjection": colorCycle(5),
+                      "HostMigration": colorCycle(6),
+                      "LinkFailure": colorCycle(7),
+                      "SwitchFailure": colorCycle(8),
+                      "LinkRecovery": colorCycle(9),
+                      "SwitchRecovery": colorCycle(10),
+                      "ControllerFailure": colorCycle(11),
+                      "ControllerRecovery": colorCycle(12),
+                      "ControlChannelBlock": colorCycle(13),
+                      "ControlChannelUnblock": colorCycle(14)};
 
     function timeline (gParent) {
       var g = gParent.append("g");
@@ -33,13 +49,20 @@
       var gParentItem = d3.select(gParent[0][0]);
 
       var yAxisMapping = {},
-        maxStack = 1,
-        minTime = 0,
-        maxTime = 0;
+          maxStack = 1,
+          minTime = 0,
+          maxTime = 0;
 
-      var feIDs = {};
+      // clear the canvas
+      d3.selectAll("circle").remove();
+      d3.selectAll("line").remove(); 
+      d3.selectAll("text").remove();
 
       setWidth();
+
+      var IDs = {}; // 1D : functionally equivalent event pairs
+                    // 2D : send/receive control message event pairs
+      var legendHeight = 0; // keep track of the last drawn entity's position in the graph to appropriately display the legend
 
       // check how many stacks we're gonna need
       // do this here so that we can draw the axis before the graph
@@ -53,22 +76,18 @@
               maxStack++;
             }
 
-            // figure out beginning and ending times if they are unspecified
-            if (ending == 0 && beginning == 0){
-              datum.times.forEach(function (time, i) {
-                if (time.starting_time < minTime || minTime == 0)
-                  minTime = time.starting_time;
-                if (time.ending_time > maxTime)
-                  maxTime = time.ending_time;
-              });
-            }
+            // always look for beginning and ending times of all timelines
+            datum.times.forEach(function (time, i) {
+              if (time.starting_time < minTime || minTime == 0)
+                minTime = time.starting_time;
+              if (time.ending_time > maxTime)
+                maxTime = time.ending_time;
+            });
           });
         });
 
-        if (ending == 0 && beginning == 0) {
-          beginning = minTime;
-          ending = maxTime;
-        }
+        beginning = minTime;
+        ending = maxTime;
       }
 
       var scaleFactor = (1/(ending - beginning)) * (width - margin.left - margin.right);
@@ -83,15 +102,13 @@
         .orient(orient)
         .tickFormat(tickFormat.format)
         .ticks(tickFormat.tickTime, tickFormat.tickNumber);
-        
+
       g.append("g")
         .attr("class", "axis")
-        .attr("transform", "translate(" + 80 +","+ (2*timelineDiffHeight+20) + ")")
-        .call(xAxis);  
+        .attr("transform", "translate(" + 80 +","+ 0+")")
+        .call(xAxis);
 
-      // draw the chart
       g.each(function(d, i) {
-
         d.forEach( function(datum, index){
           var data = datum.times;
           var hasLabel = (typeof(datum.label) != "undefined");
@@ -105,45 +122,71 @@
             })
             .attr("cy", getStackPosition)
             .attr("cx", getXPos)
-            .attr("r", itemHeight/4)
+            .attr("r", itemHeight/5)
             .attr("height", itemHeight)
-            .style("fill", colorCycle(index))
+            .style("fill", getColor)
             .on("mousemove", function (d, i) {
               hover(d, index, datum);
             })
             .on("mouseover", function (d, i) {
-              mouseover(d, i, datum);
+              mouseover(d, i, datum, IDs, g); 
             })
             .on("mouseout", function (d, i) {
-              mouseout(d, i, datum);
+              mouseout(d, i, datum, IDs, g); 
             })
             .on("click", function (d, i) {
               click(d, index, datum);
             })
           ;
 
+          // 1D: keeping track of functionally equivalent events fe_id 
+          // 2D: keeping track of send/receive events sr_id  
           for (i = 0; i < data.length; i++){
-            var id = data[i].fe_id;  
-            if ( !(id in feIDs)){
-              feIDs[id] = [getXPos(data[i],i), getStackPosition(data[i],i)];
-            } else {
-              feIDs[id].push(getXPos(data[i],i)); 
-              feIDs[id].push(getStackPosition(data[i],i));
+            var id; 
+            if (timeline_type == "1d"){ 
+              id = data[i].fe_id;
+            } 
+            else if (timeline_type == "2d"){
+              id = data[i].sr_id; 
             }
+
+            if (!(id in IDs)){ 
+              IDs[id] = [getXPos(data[i],i), getStackPosition(data[i],i)];
+            } else {
+              IDs[id].push(getXPos(data[i],i)); 
+              IDs[id].push(getStackPosition(data[i],i));
+            }
+            
           }
 
+          labelHeight = itemHeight/2 + margin.top + (itemHeight + itemMargin) * yAxisMapping[index]+ 25; 
+          // update relative position of the legend 
+          if ( labelHeight > legendHeight){
+              legendHeight = labelHeight;
+          }
+          
           // add the label
           if (hasLabel) {
             gParent.append('text')
               .attr("class", "timeline-label")
-              .attr("transform", "translate("+ 0 +","+ (itemHeight/2 + margin.top + (itemHeight + itemMargin) * yAxisMapping[index])+")")
+              .attr("transform", "translate("+ 0 +","+ labelHeight+")")
               .text(hasLabel ? datum.label : datum.id);
+
+              // 2D: draw the line for each entity beside the label
+              if (timeline_type == "2d"){
+                g.append("line")
+                  .attr("x1", margin.left + 80)
+                  .attr("y1", labelHeight - itemHeight/2)
+                  .attr("x2", width - margin.right + 80)
+                  .attr("y2", labelHeight - itemHeight/2)
+                  .style("stroke","rgb(0,0,0)");
+              }
           }
 
           if (typeof(datum.icon) != "undefined") {
             gParent.append('image')
               .attr("class", "timeline-label")
-              .attr("transform", "translate("+ 0 +","+ (margin.top + (itemHeight + itemMargin) * yAxisMapping[index])+")")
+              .attr("transform", "translate("+ 0 +","+ labelHeight +")")
               .attr("xlink:href", datum.icon)
               .attr("width", margin.left)
               .attr("height", itemHeight);
@@ -151,24 +194,70 @@
 
           function getStackPosition(d, i) {
             if (stacked) {
-              return margin.top + (itemHeight + itemMargin) * yAxisMapping[index];
+              return margin.top + (itemHeight + itemMargin) * yAxisMapping[index] + 25;
             }
-            return margin.top;
+            return margin.top + 25;
           }
-        });       
+
+          function getColor(d, i){
+            if (d.class in eventColors){
+              return eventColors[d.class];
+            }
+            return colorCycle(0);
+          }
+        });
       });
 
-      //draw red lines connecting functionally equivalent events based upon prior calculated fe_id  
-      for (var id in feIDs){
-        if (feIDs[id].length == 4){
-          g.append('line')
-            .attr("x1", feIDs[id][0])
-            .attr("y1", feIDs[id][1])
-            .attr("x2", feIDs[id][2])
-            .attr("y2", feIDs[id][3])
-            .style("stroke","rgb(255,0,0)");
+      // 2D: draw lines appropriately between send/receive messages between controllers and switches 
+      if (timeline_type == "2d"){
+        for (var id in IDs){
+          if (IDs[id].length == 4){
+            g.append('line')
+              .attr("x1", IDs[id][0])
+              .attr("y1", IDs[id][1])
+              .attr("x2", IDs[id][2])
+              .attr("y2", IDs[id][3])
+              .style("stroke","rgb(0,0,0)");          
+          }
         }
       }
+
+      // draw legend after displaying trace 
+      if (d3.selectAll("circle")[0].length != 0 ){
+        gParent.append('text')
+          .attr("class", "legend")
+          .attr("transform", "translate(" + 20 + "," + (legendHeight+50) + ")")
+          .style("font-size",13)
+          .text("Legend");
+
+        count = 0;
+        eventsPerCol = 8;
+        rowSpace = 18; 
+        colSpace = 160;
+        col = 0;
+
+        // display event type and its corresponding color
+        for(var e in eventColors){
+          count++;
+          if (count > col*eventsPerCol){
+            col++;
+          }
+          gParent.append('circle')
+            .attr('x', 40 + colSpace*(col-1))
+            .attr("y", legendHeight + 47 + rowSpace*(count%(eventsPerCol)+1))
+            .attr("cy", legendHeight + 47 + rowSpace*(count%(eventsPerCol)+1))
+            .attr("cx", 40 + colSpace*(col-1))
+            .attr("r", 3)
+            .style("fill", eventColors[e]);   
+
+          gParent.append('text')
+            .attr("class", "legend")
+            .attr("transform", "translate("+ (55 + colSpace*(col-1)) +","+ (legendHeight + 50 + rowSpace*(count%(eventsPerCol)+1))+")")
+            .style("font-size",10.5)
+            .text(e);
+        }
+      }
+
 
       if (width > gParentSize.width) {
         var zoom = d3.behavior.zoom().x(xScale).on("zoom", move);
@@ -201,6 +290,9 @@
       }
 
       function setHeight() {
+
+        height = 3000;
+
         if (!height && !gParentItem.attr("height")) {
           if (itemHeight) {
             // set height based off of item height
@@ -220,6 +312,7 @@
       }
 
       function setWidth() {
+
         if (!width && !gParentSize.width) {
           throw "width of the timeline is not set";
         } else if (!(width && gParentSize.width)) {
@@ -342,3 +435,5 @@
     return timeline;
   };
 })();
+          
+
