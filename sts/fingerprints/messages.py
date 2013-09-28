e925698c8213451b81ad25a456ed622c543a44b8
colin-scott <cs@cs.berkeley.edu>
2013-09-28 11:43:52 -0700

    Separate error messages for submodule import vs. submodule build

--- sts/fingerprints/messages.py
+++ sts/fingerprints/messages.py
@@ -19,19 +19,28 @@ from pox.lib.packet.ethernet import *
 from pox.lib.packet.lldp import *
 from pox.lib.packet.arp import *
 from pox.lib.packet.ipv4 import *
+import sys
 try:
+  # Import a dummy hsa module to check that the submodule is there.
+  import examples as hsa_import_test
+except ImportError:
+  print >> sys.stderr, str('''Headerspace submodule not loaded. '''
+                           '''flow_mod_matches will not be considered during replay. '''
+                           ''' Load with:\n'''
+                           '''$ git submodule init \n'''
+                           '''$ git submodule update \n''')
+
+try:
+  # Import the actual module to see if it's built.
   import config_parser.openflow_parser as hsa
 except ImportError:
-  import sys
-  build_instructions = str(''' $ git submodule init \n'''
-                           ''' $ git submodule update \n'''
-                           ''' $ (cd sts/hassel/hsa-python && source setup.sh) ''')
-  print >> sys.stderr, str('''Headerspace Module not loaded. '''
+  build_instructions = "$ (cd sts/hassel/hsa-python && source setup.sh)"
+  print >> sys.stderr, str('''Headerspace module not built. '''
                            '''flow_mod matches will not be considered during replay.\n'''
-                           '''Load it with:\n%s''' % build_instructions)
+                           '''Build it with:\n%s''' % build_instructions)
   class NeedSubmodule(object):
     def _need_submodule(self):
-      raise RuntimeError("Need to load the hassel submodule:\n%s" % build_instructions)
+      raise RuntimeError("Need to build the hassel submodule:\n%s" % build_instructions)
     def format(self, *args):
       self._need_submodule()
     @property
