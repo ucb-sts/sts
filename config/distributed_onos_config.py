7e79cb57a82589a0ea764a757d5a07a67abe811d
colin-scott <cs@cs.berkeley.edu>
2013-10-19 14:57:19 -0700

    Add ONOS configs and a ONOSController controller_type

---
+++ config/distributed_onos_config.py
@@ -0,0 +1,52 @@
+
+import os
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import MeshTopology
+from sts.control_flow import Fuzzer
+from sts.input_traces.input_logger import InputLogger
+from sts.simulation_state import SimulationConfig
+
+if 'CLUSTER' not in os.environ:
+  raise RuntimeError('''Need to set $CLUSTER. See '''
+                     '''https://wiki.onlab.us:8443/display/Eng/ONOS+Development+VM''')
+  # On c5:
+  # export CLUSTER=${HOME}/.cluster.hosts
+  # export ONOS_CLUSTER_BASENAME="onosdev"
+  # export ONOS_CLUSTER_NR_NODES=2
+  # export PATH=${HOME}/vagrant_onosdev/ONOS/cluster-mgmt/bin:$PATH
+
+# Use ONOS as our controller.
+# TODO(cs): first make sure to clean up any preexisting ONOS instances.
+# N.B. this command is for the entire cluster, not individual nodes.
+start_cmd = (''' vagrant up onosdev1 onosdev2 ; '''
+             ''' ./scripts/conf_setup.sh 2 ; zk start ; cassandra start; '''
+             ''' onos start ; sleep 15 ''')
+# N.B kills a single node.
+kill_cmd = (''' vagrant ssh %s -c "cd ONOS; ./start-onos.sh stop"''')
+# N.B. starts a single node.
+restart_cmd = 'vagrant ssh %s -c "cd ONOS; ./start-onos.sh start"'
+dummy_cmd = 'sleep 1'
+
+controllers = [ControllerConfig(start_cmd, address="192.168.56.11", port=6633,
+                                kill_cmd=kill_cmd % "onosdev1",
+                                restart_cmd=restart_cmd % "onosdev1",
+                                controller_type="onos",
+                                cwd="/home/rcs/vagrant_onosdev"),
+               ControllerConfig(dummy_cmd, address="192.168.56.12", port=6633,
+                                kill_cmd=kill_cmd % "onosdev2",
+                                restart_cmd=restart_cmd % "onosdev2",
+                                controller_type="onos",
+                                cwd="/home/rcs/vagrant_onosdev")]
+topology_class = MeshTopology
+topology_params = "num_switches=2"
+
+simulation_config = SimulationConfig(controller_configs=controllers,
+                                     topology_class=topology_class,
+                                     topology_params=topology_params)
+                                     #multiplex_sockets=True)
+
+control_flow = Fuzzer(simulation_config, check_interval=20,
+                      halt_on_violation=True,
+                      input_logger=InputLogger(),
+                      invariant_check_name="InvariantChecker.check_loops")
+
---
+++ config/onos_config.py
@@ -0,0 +1,37 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import MeshTopology
+from sts.control_flow import Fuzzer
+from sts.input_traces.input_logger import InputLogger
+from sts.simulation_state import SimulationConfig
+
+# Use ONOS as our controller.
+# TODO(cs): ensure that no prior ONOS instances are running.
+# N.B. ./scripts/conf_setup.sh automatically clears the cassandra image. If
+# you're running on a machine other than c5, you may have to modify
+# conf_setup.sh to remove the interactive prompt for clearing the cassandra
+# image.
+start_cmd = ('''vagrant up onosdev1 ; ./scripts/conf_setup.sh 1 ;  '''
+             ''' vagrant ssh onosdev1 -c "cd ONOS; ./start-zk.sh start; sleep 5; '''
+             ''' ./start-cassandra.sh start; sleep 10; '''
+             ''' ./start-onos.sh start; sleep 15"''')
+restart_cmd = 'vagrant ssh onosdev1 -c "cd ONOS; ./start-onos.sh start"'
+kill_cmd = 'vagrant ssh onosdev1 -c "cd ONOS; ./start-onos.sh stop"'
+
+controllers = [ControllerConfig(start_cmd, address="192.168.56.11", port=6633,
+                                restart_cmd=restart_cmd, kill_cmd=kill_cmd,
+                                controller_type="onos",
+                                cwd="/home/rcs/vagrant_onosdev")]
+topology_class = MeshTopology
+topology_params = "num_switches=2"
+
+simulation_config = SimulationConfig(controller_configs=controllers,
+                                     topology_class=topology_class,
+                                     topology_params=topology_params)
+                                     #multiplex_sockets=True)
+
+control_flow = Fuzzer(simulation_config, check_interval=20,
+                      halt_on_violation=True,
+                      input_logger=InputLogger(),
+                      invariant_check_name="InvariantChecker.check_loops")
+
