d7475c64a77c146bec6bcbe9d67e37ec81705d58
Jefferson Lai <jeffersonlai@Jeffersons-MacBook-Pro.local>
2013-10-26 05:19:34 -0700

    renamed god_scheduler_test.py to openflow_buffer_test.py

--- tests/unit/sts/openflow_buffer_test.py
+++ tests/unit/sts/openflow_buffer_test.py
@@ -20,7 +20,7 @@ sys.path.append(os.path.dirname(__file__) + "/../../..")
 
 # N.B. this import is needed to avoid a circular dependency.
 from sts.replay_event import *
-from sts.god_scheduler import GodScheduler, PendingReceive
+from sts.openflow_buffer import OpenFlowBuffer, PendingReceive
 from pox.openflow.libopenflow_01 import *
 
 class MockConnection(object):
@@ -30,15 +30,15 @@ class MockConnection(object):
   def allow_message_receipt(self, message):
     self.passed_message = True
 
-class GodSchedulerTest(unittest.TestCase):
+class OpenFlowBufferTest(unittest.TestCase):
   def test_basic(self):
-    god = GodScheduler()
+    buf = OpenFlowBuffer()
     message = ofp_flow_mod(match=ofp_match(in_port=1, nw_src="1.1.1.1"),
                            action=ofp_action_output(port=1))
     mock_conn = MockConnection()
-    god.insert_pending_receipt(1,"c1",message,mock_conn)
+    buf.insert_pending_receipt(1,"c1",message,mock_conn)
     pending_receipt = PendingReceive(1,"c1",OFFingerprint.from_pkt(message))
-    self.assertTrue(god.message_receipt_waiting(pending_receipt))
-    god.schedule(pending_receipt)
+    self.assertTrue(buf.message_receipt_waiting(pending_receipt))
+    buf.schedule(pending_receipt)
     self.assertTrue(mock_conn.passed_message)
-    self.assertFalse(god.message_receipt_waiting(pending_receipt))
+    self.assertFalse(buf.message_receipt_waiting(pending_receipt))
