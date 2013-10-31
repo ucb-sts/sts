c71e7a3d7ae47aabd54547356181cb1965be8402
Andrew Or <andrewor14@gmail.com>
2013-10-30 17:33:38 -0700

    Update tool to also verify violation with Replay

--- tools/fuzz_many_times.sh
+++ tools/fuzz_many_times.sh
@@ -24,10 +24,18 @@ fi
 EXP_NAME=$(basename $2)
 EXP_NAME=${EXP_NAME%.*}
 
+rm -rf experiments/"$EXP_NAME"_no_violations
+rm -rf experiments/"$EXP_NAME"_irreproducible
+mkdir experiments/"$EXP_NAME"_no_violations
+mkdir experiments/"$EXP_NAME"_irreproducible
+
+NUM_REPLAYS=5
+
 # Run simulator 
 for i in $(seq 1 $1)
 do
   echo -e "\n==================== Starting the $i'th iteration ====================\n"
+  # Run Fuzzer
   ./simulator.py -c $2
   NEW_EXP_NAME="$EXP_NAME"_"$i"
   mv experiments/"$EXP_NAME" experiments/"$NEW_EXP_NAME"
@@ -35,14 +43,22 @@ do
   NO_VIOLATION=$(tail -n 100 experiments/"$NEW_EXP_NAME"/simulator.out | grep "Round 500 completed")
   if [[ ! -z "$NO_VIOLATION" ]]; then
     echo "$NEW_EXP_NAME has no violations!"
-    rm -rf experiments/"$NEW_EXP_NAME"
+    mv experiments/"$NEW_EXP_NAME" experiments/"$EXP_NAME"_no_violations
     continue
   fi
-  ./simulator.py -c experiments/"$NEW_EXP_NAME"/replay_config.py
-  REPLAY_NO_VIOLATION=$(tail -n 100 experiments/"$NEW_EXP_NAME"_replay/simulator.out | grep "No correctness violations")
-  if [[ ! -z "$REPLAY_NO_VIOLATION" ]]; then
-    echo "Cannot reproduce $NEW_EXP_NAME violation!"
-    rm -rf experiments/"$NEW_EXP_NAME"
-    rm -rf experiments/"$NEW_EXP_NAME"_replay
-  fi
+  # Run Replayer up to NUM_REPLAYS times
+  for j in $(seq 1 $NUM_REPLAYS)
+  do
+    ./simulator.py -c experiments/"$NEW_EXP_NAME"/replay_config.py
+    REPLAY_NO_VIOLATION=$(tail -n 100 experiments/"$NEW_EXP_NAME"_replay/simulator.out | grep "No correctness violations")
+    if [[ -z "$REPLAY_NO_VIOLATION" ]]; then
+      break
+    fi
+    if [[ $j -eq $NUM_REPLAYS ]]; then
+      echo "Cannot reproduce $NEW_EXP_NAME violation!"
+      mv experiments/"$NEW_EXP_NAME" experiments/"$EXP_NAME"_irreproducible
+      break
+    fi
+  done
 done
+
