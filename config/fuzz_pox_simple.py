cb29841b0e52f3b5479eb072d2e0a3d3c85f97b2
colin-scott <cs@cs.berkeley.edu>
2013-11-01 17:25:01 -0700

    Add a simple fuzz config

---
+++ config/fuzz_pox_simple.py
@@ -0,0 +1,23 @@
+
+from config.experiment_config_lib import ControllerConfig
+from sts.topology import MeshTopology
+from sts.control_flow import Fuzzer
+from sts.input_traces.input_logger import InputLogger
+from sts.simulation_state import SimulationConfig
+
+# Use POX as our controller
+start_cmd = ('''./pox.py openflow.discovery forwarding.l2_multi '''
+             '''openflow.of_01 --address=__address__ --port=__port__''')
+controllers = [ControllerConfig(start_cmd, cwd="pox/")]
+
+topology_class = MeshTopology
+topology_params = "num_switches=2"
+
+simulation_config = SimulationConfig(controller_configs=controllers,
+                                     topology_class=topology_class,
+                                     topology_params=topology_params)
+
+control_flow = Fuzzer(simulation_config,
+                      input_logger=InputLogger(),
+                      invariant_check_name="InvariantChecker.check_liveness",
+                      halt_on_violation=True)
