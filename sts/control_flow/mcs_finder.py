7167d50ccf45054d5f750adb7a7738496ba0dc9a
colin-scott <cs@cs.berkeley.edu>
2013-12-27 12:18:14 -0800

    Better error messages for when users are effected by non-determinism. for when users are effected by non-determinism. for when users are effected by non-determinism. for when users are effected by non-determinism.

--- sts/control_flow/mcs_finder.py
+++ sts/control_flow/mcs_finder.py
@@ -49,7 +49,7 @@ class MCSFinder(ControlFlow):
                transform_dag=None, end_wait_seconds=0.5,
                mcs_trace_path=None, extra_log=None, runtime_stats_path=None,
                wait_on_deterministic_values=False,
-               no_violation_verification_runs=1,
+               replays_per_subsequence=1,
                optimized_filtering=False, forker=LocalForker(),
                replay_final_trace=True, strict_assertion_checking=False,
                delay_flow_mods=False,
@@ -109,7 +109,7 @@ class MCSFinder(ControlFlow):
     self.end_wait_seconds = end_wait_seconds
     self.wait_on_deterministic_values = wait_on_deterministic_values
     # `no' means "number"
-    self.no_violation_verification_runs = no_violation_verification_runs
+    self.replays_per_subsequence = replays_per_subsequence
     self._runtime_stats = RuntimeStats(self.subsequence_id, runtime_stats_path=runtime_stats_path)
     # Whether to try alternate trace splitting techiques besides splitting by time.
     self.optimized_filtering = optimized_filtering
@@ -174,7 +174,7 @@ class MCSFinder(ControlFlow):
       # First, run through without pruning to verify that the violation exists
       self._runtime_stats.record_replay_start()
 
-      for i in range(0, self.no_violation_verification_runs):
+      for i in range(0, self.replays_per_subsequence):
         bug_found = self.replay(self.dag, "reproducibility",
                                 ignore_runtime_stats=True)
         if bug_found:
@@ -224,8 +224,12 @@ class MCSFinder(ControlFlow):
                                 ignore_runtime_stats=True)
         if not bug_found:
           self.log('''Warning! Final MCS did not result in violation, even '''
-                   ''' after ignoring timed out internal events. '''
-                   ''' See tools/visualization/visualize1D.html for debugging''')
+                   '''after ignoring timed out internal events. Your run ''
+                   '' is probably effected by non-determinism.\n'''
+                   '''Try setting MCSFinder's replays_per_subsequence '''
+                   '''parameter in your config file (e.g. replays_per_subsequence=10)\n'''
+                   '''If that still doesn't work, see tools/visualization/visualize1D.html '''
+                   '''for debugging''')
 
     # N.B. dumping the MCS trace must occur after the final replay trace,
     # since we need to infer which events will time out for events.trace.notimeouts
@@ -321,8 +325,8 @@ class MCSFinder(ControlFlow):
   # N.B. always called within a child process.
   def _check_violation(self, new_dag, subset_index, label):
     ''' Check if there were violations '''
-    # Try no_violation_verification_runs times to see if the bug shows up
-    for i in range(0, self.no_violation_verification_runs):
+    # Try replays_per_subsequence times to see if the bug shows up
+    for i in range(0, self.replays_per_subsequence):
       bug_found = self.replay(new_dag, label)
 
       if bug_found:
