fe5cb54441d5ff2bc75e810bef5f7013c399385e
colin-scott <cs@cs.berkeley.edu>
2014-01-01 20:51:35 -0800

    Add SnapshotPeeker, and refactor to remove redundancy. Not yet tested

--- sts/control_flow/peeker.py
+++ sts/control_flow/peeker.py
@@ -15,10 +15,12 @@
 
 import logging
 import time
+import abc
 from collections import Counter
 
 from sts.event_dag import EventDag
 from sts.control_flow.replayer import Replayer
+from sts.control_flow.snapshot_utils import *
 from sts.replay_event import InternalEvent
 
 log = logging.getLogger("sts")
@@ -26,11 +28,14 @@ log = logging.getLogger("sts")
 # TODO(cs): is it problematic if we stop responding to OFP_PING / LLDP during
 # peek()?
 
-# TODO(cs): peek() runs this should run as subprocesses to cope with memory
+# TODO(cs): peek() should be run as a subprocesses to cope with memory
 # constraints, as in mcs_finder. We would need to refactor the class variables
 # (convert them to RPC return values, as in client_stats_dict()) below to enable this.
 
 class Peeker(object):
+  ''' Peekers are DAG `transformers' that infer the internal events that are or
+  are not going to occur for a given event subsequence. '''
+  __metaclass__ = abc.ABCMeta
   # { % of inferred fingerprints that were ambiguous ->
   #   # of replays where this % occurred }
   ambiguous_counts = Counter()
@@ -38,6 +43,112 @@ class Peeker(object):
   ambiguous_events = Counter()
 
   def __init__(self, simulation_cfg, default_wait_time_seconds=0.5, epsilon_time=0.2):
+    self.simulation_cfg = simulation_cfg
+    self.default_wait_time_seconds = default_wait_time_seconds
+    self.epsilon_time = epsilon_time
+
+  @abc.abstractmethod
+  def peek(self, dag):
+    '''
+    Return a transformed EventDag complete with the internal events that
+    are going to occur for the input sequence reflected in the given
+    EventDag
+
+    Pre: there is not a simulation (i.e. running controller) in progress
+    '''
+    pass
+
+  def get_wait_time_seconds(self, first_event, second_event):
+    if first_event is None or second_event is None:
+      return self.default_wait_time_seconds
+    else:
+      return second_event.time.as_float() - first_event.time.as_float() + \
+             self.epsilon_time
+
+
+class SnapshotPeeker(Peeker):
+  ''' O(n) peeker that takes controller snapshots at each input, peeks
+  forward, then restarts the snapshot up until the next input'''
+  def __init__(self, simulation_cfg, default_wait_time_seconds=0.5, epsilon_time=0.2):
+    if len(simulation_cfg.controller_configs) != 1:
+      raise ValueError("Only one controller supported for snapshotting")
+    if simulation_cfg.controller_configs[0].sync is not None:
+      raise ValueError("STSSyncProto currently incompatible with snapshotting")
+    super(SnapshotPeeker, self).__init__(simulation_cfg,
+                                         default_wait_time_seconds=default_wait_time_seconds,
+                                         epsilon_time=epsilon_time)
+
+  def peek(self, dag):
+    # Inferred events includes input events and internal events
+    inferred_events = []
+
+    # TODO(cs): currently assumes that STSSyncProto is not used alongside
+    # snapshotting.
+    simulation = simulation_config.bootstrap(RecordingSyncCallback(None))
+    simulation.openflow_buffer.pass_through_whitelisted_messages = True
+    simulation.connect_to_controllers()
+    controller = simulation.controller_manager.controllers[0]
+
+    # Internal events inferred in the previous iteration of the following for
+    # loop
+    events_inferred_last_iteration = []
+
+    # Note that there may be internal events before the first input, so we
+    # start the input index at -1
+    for inject_input_idx in xrange(-1, len(dag.input_events)):
+      log.debug("peek()'ing after input %d" % (inject_input_idx))
+
+      inject_input = get_inject_input(inject_input_idx, dag.input_events)
+      following_input = get_following_input(inject_input_idx, dag.input_events)
+      expected_internal_events = \
+         get_expected_internal_events(inject_input, following_input, dag.events)
+
+      if expected_internal_events == []:
+        # Optimization: if no internal events occured between this input and the
+        # next, no need to peek(), just replay the next input
+        log.debug("Optimization: no expected internal events")
+        Peeker.ambiguous_counts[0.0] += 1
+        events_inferred_last_iteration = []
+        if inject_input is not None:
+          inferred_events.append(inject_input)
+        continue
+
+      # Play forward the internal events inferred from last round and the
+      # input to be injected
+      if inject_input is None:
+        wait_time = WaitTime(following_input.time.as_float() -
+                             dag.events[0].time.as_float())
+        dag_interval = EventDag([wait_time])
+      else:
+        dag_interval = EventDag(events_inferred_last_iteration + [inject_input])
+
+      assert(dag_interval.events != [])
+      replayer = Replayer(self.simulation_cfg, dag_interval,
+                          pass_through_whitelisted_messages=True)
+      replay.run_simulation_forward()
+
+      # Now peek() for internal events following inject_input
+      snapshot_controller(simulation, controller)
+
+      wait_time_seconds = self.get_wait_time_seconds(inject_input, following_input)
+      found_events = play_forward(simulation, wait_time_seconds)
+      events_inferred_last_iteration = match_and_filter(found_events, expected_internal_events)
+
+      # Finally, bring the controller back to the state it was at just after
+      # injecting inject_input
+      snapshot_proceed(simulation, controller)
+
+      inferred_events += events_inferred_last_iteration
+      if inject_input is not None:
+        inferred_events.append(inject_input)
+
+
+class PrefixPeeker(Peeker):
+  ''' O(n^2) peeker that replays each prefix of the subseqeuence from the beginning. '''
+  def __init__(self, simulation_cfg, default_wait_time_seconds=0.5, epsilon_time=0.2):
+    super(PrefixPeeker, self).__init__(simulation_cfg,
+                                       default_wait_time_seconds=default_wait_time_seconds,
+                                       epsilon_time=epsilon_time)
     try:
       import pytrie
     except ImportError:
@@ -45,13 +156,11 @@ class Peeker(object):
     # The prefix trie stores lists of input events as keys,
     # and lists of both input and internal events as values
     # Note that we pass the trie around between DAG views
-    self.simulation_cfg = simulation_cfg
     self._prefix_trie = pytrie.Trie()
-    self.default_wait_time_seconds = default_wait_time_seconds
-    self.epsilon_time = epsilon_time
 
   def peek(self, dag):
-    ''' Infer which internal events are/aren't going to occur, '''
+    ''' Infer which internal events are/aren't going to occur (for the entire
+    sequence of input events in dag)'''
     # TODO(cs): optimization: write the prefix trie to a file, in case we want to run
     # FindMCS again?
     input_events = dag.input_events
@@ -81,16 +190,10 @@ class Peeker(object):
     while inject_input_idx < len(input_events):
       # The input we're about to inject
       inject_input = input_events[inject_input_idx]
-
-      if inject_input_idx < len(input_events) - 1:
-        # there is a following input_event
-        following_input = input_events[inject_input_idx + 1]
-      else:
-        following_input = None
+      following_input = get_following_input(inject_input_idx, dag.input_events)
 
       # The input following the one we're going to inject
-      log.debug("peek()'ing after input %d" %
-                (inject_input_idx))
+      log.debug("peek()'ing after input %d" % (inject_input_idx))
 
       expected_internal_events = \
         get_expected_internal_events(inject_input, following_input, dag.events)
@@ -105,7 +208,7 @@ class Peeker(object):
         wait_time_seconds = self.get_wait_time_seconds(inject_input, following_input)
         replay_dag = EventDag(inferred_events + [ inject_input ])
         found_events = self.find_internal_events(replay_dag, wait_time_seconds)
-        newly_inferred_events = self.match_and_filter(found_events, expected_internal_events)
+        newly_inferred_events = match_and_filter(found_events, expected_internal_events)
 
       (current_input_prefix,
        inferred_events) = self._update_trie(current_input_prefix, inject_input,
@@ -114,57 +217,14 @@ class Peeker(object):
 
     return EventDag(inferred_events)
 
-  def get_wait_time_seconds(self, first_event, second_event):
-    if first_event is None or second_event is None:
-      return self.default_wait_time_seconds
-    else:
-      return second_event.time.as_float() - first_event.time.as_float() + \
-          self.epsilon_time
-
   def find_internal_events(self, replay_dag, wait_time_seconds):
     ''' Replay the replay_dag, then wait for wait_time_seconds and collect internal
         events that occur. Return the list of internal events. '''
     replayer = Replayer(self.simulation_cfg, replay_dag)
     log.debug("Replaying prefix")
     simulation = replayer.simulate()
-
-    # Directly after the last input has been injected, flush the internal
-    # event buffers in case there were unaccounted internal events
-    # Note that there isn't a race condition between flush()'ing and
-    # incoming internal events, since sts is single-threaded
-    # TODO(cs): flush() is not longer needed!
-    simulation.openflow_buffer.flush()
-    simulation.controller_sync_callback.flush()
-
-    # Now set all internal event buffers (GodScheduler for
-    # ControlMessageReceives and ReplaySyncCallback for state changes)
-    # to "pass through + record"
-    simulation.set_pass_through()
-
-    # Note that this is the monkey patched version of time.sleep
-    log.debug("peek()'ing for %f seconds" % wait_time_seconds)
-    time.sleep(wait_time_seconds)
-
-    # Now turn off those pass-through and grab the inferred events
-    newly_inferred_events = simulation.unset_pass_through()
+    newly_inferred_events = play_forward(simulation, wait_time_seconds)
     simulation.clean_up()
-    return newly_inferred_events
-
-  def match_and_filter(self, newly_inferred_events, expected_internal_events):
-    log.debug("Matching fingerprints")
-    log.debug("Expected: %s" % str(expected_internal_events))
-    log.debug("Inferred: %s" % str(newly_inferred_events))
-    # TODO(cs): currently not calling this, out of paranoia. May inadvertently
-    # prune expected internal events -- largely serves as an optimization
-    #newly_inferred_events = match_fingerprints(newly_inferred_events,
-    #                                           expected_internal_events)
-    # TODO(cs): need to prune any successors of e_i, in case we waited too
-    # long
-    count_overlapping_fingerprints(newly_inferred_events,
-                                   expected_internal_events)
-    newly_inferred_events = correct_timestamps(newly_inferred_events,
-                                               expected_internal_events)
-    log.debug("Matched events: %s" % str(newly_inferred_events))
     return newly_inferred_events
 
   def _update_trie(self, current_input_prefix, inject_input, inferred_events,
@@ -178,6 +238,23 @@ class Peeker(object):
     inferred_events += newly_inferred_events
     self._prefix_trie[current_input_prefix] = inferred_events
     return (current_input_prefix, inferred_events)
+
+def get_inject_input(inject_input_index, input_events):
+  ''' Return the input at inject_input_index, or None if
+  index is negative'''
+  if inject_input_idx < 0:
+    inject_input = None
+  else:
+    inject_input = input_events[inject_input_idx]
+
+def get_following_input(inject_input_index, input_events):
+  ''' Return the event following inject_input_index, or None if
+  inject_input_index is the last input '''
+  if inject_input_idx < len(input_events) - 1:
+    # there is a following input_event
+    return input_events[inject_input_idx + 1]
+  else:
+    return None
 
 def get_expected_internal_events(left_input, right_input, events_list):
   ''' Return previously observed internal events between the left_input and
@@ -199,6 +276,45 @@ def get_expected_internal_events(left_input, right_input, events_list):
 
   return [ i for i in events_list[left_idx:right_idx]
            if isinstance(i, InternalEvent) ]
+
+def play_forward(simulation, wait_time_seconds):
+  # Directly after the last input has been injected, flush the internal
+  # event buffers in case there were unaccounted internal events
+  # Note that there isn't a race condition between flush()'ing and
+  # incoming internal events, since sts is single-threaded
+  # TODO(cs): flush() is no longer needed!
+  simulation.openflow_buffer.flush()
+  simulation.controller_sync_callback.flush()
+
+  # Now set all internal event buffers (GodScheduler for
+  # ControlMessageReceives and ReplaySyncCallback for state changes)
+  # to "pass through + record"
+  simulation.set_pass_through()
+
+  # Note that this is the monkey patched version of time.sleep
+  log.debug("peek()'ing for %f seconds" % wait_time_seconds)
+  time.sleep(wait_time_seconds)
+
+  # Now turn off those pass-through and grab the inferred events
+  newly_inferred_events = simulation.unset_pass_through()
+  return newly_inferred_events
+
+def match_and_filter(newly_inferred_events, expected_internal_events):
+  log.debug("Matching fingerprints")
+  log.debug("Expected: %s" % str(expected_internal_events))
+  log.debug("Inferred: %s" % str(newly_inferred_events))
+  # TODO(cs): currently not calling this, out of paranoia. May inadvertently
+  # prune expected internal events -- largely serves as an optimization
+  #newly_inferred_events = match_fingerprints(newly_inferred_events,
+  #                                           expected_internal_events)
+  # TODO(cs): need to prune any successors of e_i, in case we waited too
+  # long
+  count_overlapping_fingerprints(newly_inferred_events,
+                                 expected_internal_events)
+  newly_inferred_events = correct_timestamps(newly_inferred_events,
+                                             expected_internal_events)
+  log.debug("Matched events: %s" % str(newly_inferred_events))
+  return newly_inferred_events
 
 def count_overlapping_fingerprints(newly_inferred_events,
                                    expected_internal_events):
