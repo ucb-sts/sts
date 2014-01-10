b8befa4a3d374b4cf811e7dcae58a2d10e20a4d4
colin-scott <cs@cs.berkeley.edu>
2014-01-09 17:44:02 -0800

    Fix snapshot_demo config

--- config/snapshot_demo.py
+++ config/snapshot_demo.py
@@ -1,14 +1,15 @@
 
 from config.experiment_config_lib import ControllerConfig
 from sts.topology import MeshTopology
-from sts.control_flow import Fuzzer
+from sts.control_flow import EfficientMCSFinder
 from sts.input_traces.input_logger import InputLogger
 from sts.simulation_state import SimulationConfig
+from sts.control_flow.peeker import SnapshotPeeker
 
 # Use POX as our controller
 start_cmd = ('''./pox.py --verbose '''
              '''openflow.discovery forwarding.l2_multi '''
-             '''sts.util.socket_mux.pox_monkeypatcher --snapshot_address="../snapshot_socket"'''
+             '''sts.util.socket_mux.pox_monkeypatcher --snapshot_address=../snapshot_socket'''
              '''openflow.of_01 --address=__address__ --port=__port__''')
 
 controllers = [ControllerConfig(start_cmd, cwd="pox", snapshot_address="./snapshot_socket")]
@@ -20,7 +21,9 @@ simulation_config = SimulationConfig(controller_configs=controllers,
                                      topology_params=topology_params,
                                      multiplex_sockets=True)
 
-control_flow = Fuzzer(simulation_config, check_interval=20,
-                      halt_on_violation=True,
-                      input_logger=InputLogger(),
-                      invariant_check_name="InvariantChecker.check_liveness")
+peeker = SnapshotPeeker(simulation_config)
+control_flow = EfficientMCSFinder(simulation_config, "/PATH/TO/EVENT.TRACE",
+                                  wait_on_deterministic_values=False,
+                                  delay_flow_mods=False,
+                                  invariant_check_name="InvariantChecker.python_check_loops",
+                                  transform_dag=peeker.peek)
