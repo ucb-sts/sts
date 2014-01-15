69b45f0861c3b763a3e155cdac3ce8e4952fc633
colin-scott <cs@cs.berkeley.edu>
2014-01-15 14:56:40 -0800

    Print OFP packet type in Stats

--- tools/trace_utils.py
+++ tools/trace_utils.py
@@ -28,8 +28,10 @@ def parse_event_trace(trace_path):
 
 class Stats(object):
   def __init__(self):
-    self.input_events = {}
-    self.internal_events = {}
+    self.input_events = Counter()
+    self.internal_events = Counter()
+    self.message_receives = Counter()
+    self.message_sends = Counter()
 
   def update(self, event):
     if isinstance(event, replay_events.InputEvent):
@@ -38,6 +40,12 @@ class Stats(object):
     else:
       event_name = str(event.__class__.__name__)
       self.internal_events[event_name] += 1
+      if event_name == "ControlMessageReceive":
+        pkt_class = event.get_packet().__class__.__name__
+        self.message_receives[pkt_class] += 1
+      elif event_name == "ControlMessageSend":
+        pkt_class = event.get_packet().__class__.__name__
+        self.message_sends[pkt_class] += 1
 
   @property
   def input_event_count(self):
@@ -61,11 +69,17 @@ class Stats(object):
     s = "Events: %d total (%d input, %d internal).\n" % (self.total_event_count, self.input_event_count, self.internal_event_count)
     if len(self.input_events) > 0:
       s += "\n\tInput events:\n"
-      for event_name, count in self.input_events.items():
+      for event_name, count in self.input_events.iteritems():
         s += "\t  %s : %d\n" % (event_name, count)
     if len(self.internal_events) > 0:
       s += "\n\tInternal events:\n"
-      for event_name, count in self.internal_events.items():
+      for event_name, count in self.internal_events.iteritems():
         s += "\t  %s : %d\n" % (event_name, count)
+        if event_name == "ControlMessageReceive":
+          for pkt_class, c in self.message_receives.iteritems():
+            s += "\t\t  %s : %d\n" % (pkt_class, c)
+        if event_name == "ControlMessageSend":
+          for pkt_class, c in self.message_sends.iteritems():
+            s += "\t\t  %s : %d\n" % (pkt_class, c)
     return s
 
