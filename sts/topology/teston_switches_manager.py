b0bf2d29b76364e0338fa85ef293771b945fb583
Ahmed El-Hassany <ahassany@indiana.edu>
2014-07-01 11:58:08 -0700

    Add Switches manager specific to TestON

---
+++ sts/topology/teston_switches_manager.py
@@ -0,0 +1,165 @@
+# Copyright 2014      Ahmed El-Hassany
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""
+TestON Specific switches manager.
+"""
+
+from collections import Iterable
+import logging
+import re
+
+from sts.entities.teston_entities import TestONOVSSwitch
+from sts.entities.teston_entities import TestONPort
+
+from sts.topology.switches_manager import SwitchManagerAbstractClass
+from sts.topology.switches_manager import SwitchManagerPolicy
+
+
+LOG = logging.getLogger("sts.topology.teston_sw_mgm")
+
+
+class TestONSwitchesManager(SwitchManagerAbstractClass):
+
+  def __init__(self, teston_mn):
+    policy = SwitchManagerPolicy(
+      can_add_switch=False, can_create_switch=False, can_crash_switch=False,
+      can_remove_switch=False, can_recover_switch=False)
+    super(TestONSwitchesManager, self).__init__(policy)
+    self.log = LOG
+    self._switches = set()
+    self.teston_mn = teston_mn
+    self._read_switches()
+
+  def _read_ports(self, node_name):
+    response = self.teston_mn.getInterfaces(node_name)
+    ports = []
+    for line in response.split("\n"):
+      if not line.startswith("name="):
+        continue
+      port_vars = {}
+      for var in line.split(","):
+        key, value = var.split("=")
+        port_vars[key] = value
+      # TODO (AH): Read port status
+      isUp = port_vars.pop('isUp', True)
+      self.log.info("Reading switch port %s(%s)" % (port_vars['name'],
+                                                    port_vars['mac']))
+      tmp = TestONPort(hw_addr=port_vars['mac'], ips=port_vars['ip'],
+                       name=port_vars['name'])
+      ports.append((tmp))
+    return ports
+
+  def _read_switches(self):
+    """
+    Read switches from the Mininet driver.
+    """
+    # Regex patterns to parse dump output
+    # Example Switch:
+    # <OVSSwitch s1: lo:127.0.0.1,s1-eth1:None,s1-eth2:None,s1-eth3:None pid=5238>
+    sw_re = r"<OVSSwitch\s(?P<name>[^:]+)\:\s(?P<ports>([^,]+,)*[^,\s]+)"
+    # Get mininet dump
+    dump = self.teston_mn.dump().split("\n")
+    for line in dump:
+      if line.startswith("<OVSSwitch"):
+        result = re.search(sw_re, line, re.I)
+        name = result.group('name')
+        dpid = self.teston_mn.getSwitchDPID(name)
+        print name, "DPID", dpid
+        self.log.info("Reading switch %s(%s)" % (name, dpid))
+        ports = self._read_ports(name)
+        switch = TestONOVSSwitch(dpid, name, ports)
+        # Todo read connected controllers
+        self._switches.add(switch)
+
+  @property
+  def switches(self):
+    return self._switches
+
+  @property
+  def live_switches(self):
+    """Returns set of live switches (or what is suppose to live)"""
+    return self.switches
+
+  @property
+  def failed_switches(self):
+    """Returns set of crashed switches (or what is suppose to crashed)"""
+    return set()
+
+  @property
+  def up_switches(self):
+    """
+    Returns set of UP switches.
+    This method should check the actual status of the switches.
+    """
+    return self.switches
+
+  @property
+  def down_switches(self):
+    """
+    Returns set of dead switches.
+    This method should check the actual status of the switches.
+    """
+    return set()
+
+  @property
+  def live_edge_switches(self):
+    """Return the switches which are currently up and can connect to hosts"""
+    return set()
+
+  def get_switch(self, switch):
+    if switch in self.switches:
+      return switch
+    for sw in self.switches:
+      if sw.name == switch:
+        return sw
+    return None
+
+  def has_switch(self, switch):
+    return self.get_switch(switch) is not None
+
+  def create_switch(self, switch_id, num_ports, can_connect_to_endhosts=True):
+    assert self._policy.can_create_switch
+    raise NotImplementedError()
+
+  def add_switch(self, switch):
+    """Adds switch to be managed by this manager"""
+    assert self._policy.can_add_switch
+    raise NotImplementedError()
+
+  def remove_switch(self, switch):
+    """Removes switch from this manager"""
+    assert self._policy.can_remove_switch
+    raise NotImplementedError()
+
+  def crash_switch(self, switch):
+    assert self._policy.can_crash_switch
+    raise NotImplementedError()
+
+  def connect_to_controllers(self, switch, controllers,
+                             max_backoff_seconds=512):
+    """
+    Connect a switch to a list (of one or more) controllers
+    """
+    assert self._policy.can_connect_to_controllers
+    assert switch in self.switches
+    if not isinstance(controllers, Iterable):
+      controllers = [controllers]
+    for controller in controllers:
+      switch.connect(controller, max_backoff_seconds=max_backoff_seconds)
+
+  def recover_switch(self, switch, controllers=None):
+    assert self._policy.can_recover_switch
+    raise NotImplementedError()
---
+++ tests/unit/sts/topology/teston_switches_manager_test.py
@@ -0,0 +1,120 @@
+# Copyright 2014 Ahmed El-Hassany
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mock
+import unittest
+
+
+from sts.topology.teston_switches_manager import TestONSwitchesManager
+
+
+
+class TestONSwitchesManagerTest(unittest.TestCase):
+  def _mock_teston(self):
+    mininet_dump = """<Host h1: h1-eth0:10.0.0.1 pid=26370>
+<Host h2: h2-eth0:10.0.0.2 pid=26371>
+<Host h3: h3-eth0:10.0.0.3 pid=26372>
+<Host h4: h4-eth0:10.0.0.4 pid=26373>
+<Host h5: h5-eth0:10.0.0.5 pid=26374>
+<Host h6: h6-eth0:10.0.0.6 pid=26375>
+<Host h7: h7-eth0:10.0.0.7 pid=26376>
+<Host h8: h8-eth0:10.0.0.8 pid=26377>
+<Host h9: h9-eth0:10.0.0.9 pid=26378>
+<OVSSwitch s1: lo:127.0.0.1,s1-eth1:None,s1-eth2:None,s1-eth3:None pid=26381>
+<OVSSwitch s2: lo:127.0.0.1,s2-eth1:None,s2-eth2:None,s2-eth3:None,s2-eth4:None pid=26386>
+<OVSSwitch s3: lo:127.0.0.1,s3-eth1:None,s3-eth2:None,s3-eth3:None,s3-eth4:None pid=26391>
+<OVSSwitch s4: lo:127.0.0.1,s4-eth1:None,s4-eth2:None,s4-eth3:None,s4-eth4:None pid=26396>
+<RemoteController c0: 127.0.0.1:6633 pid=26363>
+    """
+
+    s1_ports = """
+, i.isUp()) for i in s1.intfs.values()])p=%s" % (i.name, i.MAC(), i.IP()
+name=lo,mac=None,ip=127.0.0.1,isUp=True
+name=s1-eth1,mac=ce:c5:1e:ee:36:b4,ip=None,isUp=True
+name=s1-eth2,mac=de:29:d4:1c:4d:a1,ip=None,isUp=True
+name=s1-eth3,mac=b6:2e:aa:c3:2e:0d,ip=None,isUp=True
+mininet>
+    """
+
+    s2_ports = """
+, i.isUp()) for i in s2.intfs.values()])p=%s" % (i.name, i.MAC(), i.IP()
+name=lo,mac=None,ip=127.0.0.1,isUp=True
+name=s2-eth1,mac=3e:cd:cd:bc:d0:bc,ip=None,isUp=True
+name=s2-eth2,mac=76:ea:fc:0c:dd:f2,ip=None,isUp=True
+name=s2-eth3,mac=5e:2b:cc:f5:a2:e5,ip=None,isUp=True
+name=s2-eth4,mac=42:b2:02:de:49:5c,ip=None,isUp=True
+mininet>
+    """
+
+    s3_ports = """
+, i.isUp()) for i in s3.intfs.values()])p=%s" % (i.name, i.MAC(), i.IP()
+name=lo,mac=None,ip=127.0.0.1,isUp=True
+name=s3-eth1,mac=66:c8:b8:3a:d5:c0,ip=None,isUp=True
+name=s3-eth2,mac=16:97:73:d7:43:8a,ip=None,isUp=True
+name=s3-eth3,mac=96:46:1e:cc:26:36,ip=None,isUp=True
+name=s3-eth4,mac=2a:d3:7e:8a:22:72,ip=None,isUp=True
+mininet>
+    """
+
+    s4_ports = """
+, i.isUp()) for i in s4.intfs.values()])p=%s" % (i.name, i.MAC(), i.IP()
+name=lo,mac=None,ip=127.0.0.1,isUp=True
+name=s4-eth1,mac=4a:82:af:b3:dd:bf,ip=None,isUp=True
+name=s4-eth2,mac=fa:30:f4:61:c7:c2,ip=None,isUp=True
+name=s4-eth3,mac=c2:8f:63:d1:27:f9,ip=None,isUp=True
+name=s4-eth4,mac=5e:c8:ae:c9:2c:fc,ip=None,isUp=True
+mininet>
+    """
+
+    def getInterfaces(name):
+      if name == 's1':
+        return s1_ports
+      elif name == 's2':
+        return s2_ports
+      elif name == 's3':
+        return s3_ports
+      elif name == 's4':
+        return s4_ports
+      else:
+        raise ValueError("No ports were mocked for switch: %s" % name)
+
+    def getSwitchDPID(name):
+      if name == 's1':
+        return 2
+      elif name == 's2':
+        return 2
+      elif name == 's3':
+        return 3
+      elif name == 's4':
+        return 4
+      else:
+        raise ValueError("No DPID mocked for switch: %s" % name)
+    mn_driver = mock.Mock(name='TestONMininetDriver')
+    mn_driver.dump.return_value = mininet_dump
+    mn_driver.getInterfaces.side_effect = getInterfaces
+    mn_driver.getSwitchDPID.side_effect = getSwitchDPID
+    return mn_driver
+
+  def test_read(self):
+    # Arrange
+    mn_driver = self._mock_teston()
+    # Act
+    sw_mgm = TestONSwitchesManager(mn_driver)
+    # Assert
+    self.assertEquals(len(sw_mgm.switches), 4)
+    for switch in sw_mgm.switches:
+      if switch.name == 's1':
+        self.assertEquals(len(switch.ports), 4, switch.ports)
+      else:
+        self.assertEquals(len(switch.ports), 5, switch.ports)
